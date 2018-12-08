20ed601032b8d388844c1bacd9e1bff86a4d2233
Joel Svensson <svenssonjoel@yahoo.se>
2018-12-08 17:32:50 +0100

    adding tests for eval_cps. 3 fails

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -47,5 +47,23 @@ for lisp in *.lisp; do
     echo "------------------------------------------------------------"
 done    
 
+for lisp in *.lisp; do
+    ./test_lisp_code_cps $lisp
+
+    result=$?
+    
+    echo "------------------------------------------------------------"
+    if [ $result -eq 1 ]
+    then
+	success_count=$((success_count+1))
+	echo $lisp SUCCESS
+    else
+	fail_count=$((fail_count+1))
+	echo $lisp FAILED
+    fi
+    echo "------------------------------------------------------------"
+done    
+
+
 echo Tests passed: $success_count
 echo Tests failed: $fail_count
