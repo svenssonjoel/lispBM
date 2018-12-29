8fc5485ff690ec688fb037f0ce8a6e270a28e397
Joel Svensson <svenssonjoel@yahoo.se>
2018-12-29 13:57:20 +0100

    cleaning

--- include/heap.h
+++ include/heap.h
@@ -19,125 +19,125 @@
 #define HEAP_H_
 
 #include <stdlib.h>
-#include <stdint.h> 
+#include <stdint.h>
 
-/* 
-Planning for a more space efficient heap representation. 
-TODO: Need to find a good reference to read up on this. 
-      - List based heap 
-      - Easy to implement and somewhat efficient 
+/*
+Planning for a more space efficient heap representation.
+TODO: Need to find a good reference to read up on this.
+      - List based heap
+      - Easy to implement and somewhat efficient
 
 0000 0000  Size    Free bits
-003F FFFF  4MB      10 
+003F FFFF  4MB      10
 007F FFFF  8MB       9
 00FF FFFF  16MB      8
-01FF FFFF  32MB      7 
-03FF FFFF  64MB      6         * Kind of heap size I am looking for 
+01FF FFFF  32MB      7
+03FF FFFF  64MB      6         * Kind of heap size I am looking for
 07FF FFFF  128MB     5
-0FFF FFFF  256MB     4 
-1FFF FFFF  512MB     3    
+0FFF FFFF  256MB     4
+1FFF FFFF  512MB     3
 
-it is also the case that not all addresses will be used if all "cells" are 
-of the same size, 8 bytes... 
+it is also the case that not all addresses will be used if all "cells" are
+of the same size, 8 bytes...
 
-value 0: 0000 0000 
+value 0: 0000 0000
 value 1: 0000 0008
-value 3: 0000 0010 
+value 3: 0000 0010
 value 4: 0000 0018
 
 Means bits 0,1,2 will always be empty in a valid address.
 
 Cons cells also need to be have room for 2 pointers. So each allocated cell from
-memory should be 8bytes. 
- 
+memory should be 8bytes.
+
 Things that needs to be represented within these bits:
- 
- - GC MARK one per cell  
+
+ - GC MARK one per cell
  - TYPE: type of CAR and type of cons
 
-Types I would want: 
- - Full 32bit integer. Does not leave room for identification of type  
+Types I would want:
+ - Full 32bit integer. Does not leave room for identification of type
  - Float values.  Same problem
- 
 
-Free bits in pointers 64MB heap: 
+
+Free bits in pointers 64MB heap:
 31 30 29 28 27 26                               2 1 0
-0  0  0  0  0  0  XX XXXX XXXX XXXX XXXX XXXX X 0 0 0 
+0  0  0  0  0  0  XX XXXX XXXX XXXX XXXX XXXX X 0 0 0
 
 
-Information needed for each cell: 
- Meaning  |   bits total | bits per car | bits per cdr 
+Information needed for each cell:
+ Meaning  |   bits total | bits per car | bits per cdr
  GC mark  |    2         |   1          |   1          - only one of them will be used (the other is wasted)   
- Type     |    2x        |   x          |   x 
- Ptr/!ptr |    2         |   1          |   1 
+ Type     |    2x        |   x          |   x
+ Ptr/!ptr |    2         |   1          |   1
 
 
-Types (unboxed): 
- - Symbols 
+Types (unboxed):
+ - Symbols
  - 28bit integer   ( will need signed shift right functionality )
- - 28bit unsigned integer 
+ - 28bit unsigned integer
  - Character
 
 If four types is all that should be possible (unboxed). then 2 bits are needed to differentiate.  
-2 + 1 + 1 = 4 => 28bits for data. 
+2 + 1 + 1 = 4 => 28bits for data.
 
 bit 0: ptr/!ptr
 bit 1: gc
-bit 2-3: type (if not ptr)  
+bit 2-3: type (if not ptr)
 bit 3 - 24 ptr (if ptr)
-bit 4 - 31 value (if value) 
+bit 4 - 31 value (if value)
 
-An unboxed value can occupy a car or cdr field in a cons cell. 
+An unboxed value can occupy a car or cdr field in a cons cell.
 
 types (boxed) extra information in pointer to cell can contain information
- - 32 bit integer 
- - 32 bit unsigned integer 
- - 32 bit float 
+ - 32 bit integer
+ - 32 bit unsigned integer
+ - 32 bit float
 
-boxed representation: 
-  [ptr| cdr] 
+boxed representation:
+  [ptr| cdr]
     |
-  [Value | Aux + GC_MARK] 
+  [Value | Aux + GC_MARK]
 
 Kinds of pointers:
- - Pointer to cons cell. 
+ - Pointer to cons cell.
  - Pointer to unboxed value  (fixnums not in a list, I hope this is so rare that it can be removed )  
    - integer
-   - unsigned integer 
+   - unsigned integer
    - symbol
    - float
- - Pointer to boxed value.  
+ - Pointer to boxed value.
    - 32 bit integer
-   - 32 bit unsigned integer 
-   - 32 bit float 
+   - 32 bit unsigned integer
+   - 32 bit float
  - (Maybe something else ? Vectors/strings allocated in memory not occupied by heap?)
-   - vector of int 
-   - vector of uint 
+   - vector of int
+   - vector of uint
    - vector of float
-   - vector of double 
+   - vector of double
    - String
 
 13 pointer"types" -> needs 4 bits
 for 64MB heap there are 6 free bits. So with this scheme going to 128MB or 256MB heap 
-is also possible 
+is also possible
 
- a pointer to some off heap vector/string could be represented by 
- 
- [ptr | cdr] 
-   | 
- [full pointer | Aux + GC_MARK] 
-   | 
- [VECTOR] 
+ a pointer to some off heap vector/string could be represented by
+
+ [ptr | cdr]
+   |
+ [full pointer | Aux + GC_MARK]
+   |
+ [VECTOR]
 
 Aux bits could be used for storing vector size. Up to 30bits should be available there
 >> This is problematic. Now the information that something is a vector is split up 
 >> between 2 cons cells. This means GC needs both of these intact to be able to make 
->> proper decision. 
+>> proper decision.
 >> Will try to resolve this by adding some special symbols. But these must be symbols 
->> that cannot occur normally in programs. Then an array could be: 
+>> that cannot occur normally in programs. Then an array could be:
 
- [Full pointer | ARRAY_SYM + GC_MARK] 
-     | 
+ [Full pointer | ARRAY_SYM + GC_MARK]
+     |
  [VECTOR]
 
 
@@ -146,9 +146,9 @@ Aux bits could be used for storing vector size. Up to 30bits should be available
 1111 AA00 0000 0000 0000 0000 0000 0000   : 0xFC00 0000 (AA bits left unused for now, future heap growth?)
  */
 
-#define CONS_CELL_SIZE       8 
+#define CONS_CELL_SIZE       8
 #define ADDRESS_SHIFT        3
-#define VAL_SHIFT            4 
+#define VAL_SHIFT            4
 
 #define PTR_MASK             0x00000001
 #define PTR                  0x00000001
@@ -165,7 +165,7 @@ Aux bits could be used for storing vector size. Up to 30bits should be available
 #define PTR_TYPE_STREAM      0xF0000000
 
 #define GC_MASK              0x00000002
-#define GC_MARKED            0x00000002 
+#define GC_MARKED            0x00000002
 
 #define VAL_MASK             0xFFFFFFF0
 #define VAL_TYPE_MASK        0x0000000C
@@ -199,17 +199,17 @@ Aux bits could be used for storing vector size. Up to 30bits should be available
 
 typedef struct {
   uint32_t car;
-  uint32_t cdr; 
+  uint32_t cdr;
 } cons_t;
 
 typedef struct {
   uint32_t heap_base;          // address of heap in memory
   uint32_t freelist;           // list of free cons cells.
   uint32_t freelist_last;      // points at the last element in the free list
-  
+
   uint32_t heap_size;          // In number of cells.
   uint32_t heap_bytes;         // Size in bytes.
-  
+
   uint32_t num_alloc;          // Number of cells allocated.
   uint32_t num_alloc_arrays;   // Number of arrays allocated.
 
@@ -229,8 +229,8 @@ typedef struct {
     uint32_t *u32;
     int32_t  *i32;
     char     *c;
-  } data;                   // Array data storage 
-} array_t;                 
+  } data;                   // Array data storage
+} array_t;
 
 typedef struct {
   uint32_t aux;             // GC mark
@@ -252,8 +252,8 @@ extern int heap_init(size_t num_cells);
 extern void heap_del(void);
 extern uint32_t heap_num_free(void);
 extern uint32_t heap_num_allocated(void);
-extern uint32_t heap_size(); 
-extern uint32_t heap_allocate_cell(uint32_t type); 
+extern uint32_t heap_size();
+extern uint32_t heap_allocate_cell(uint32_t type);
 extern uint32_t heap_size_bytes(void);
 
 extern uint32_t cons(uint32_t car, uint32_t cdr);
@@ -261,18 +261,18 @@ extern uint32_t car(uint32_t cons);
 extern uint32_t cdr(uint32_t cons);
 extern void set_car(uint32_t c, uint32_t v);
 extern void set_cdr(uint32_t c, uint32_t v);
-extern uint32_t length(uint32_t c); 
+extern uint32_t length(uint32_t c);
 extern uint32_t reverse(uint32_t list);
 
 // State and statistics
 extern void heap_get_state(heap_state_t *);
 
 // Garbage collection
-extern int heap_perform_gc(uint32_t env); 
+extern int heap_perform_gc(uint32_t env);
 extern int heap_perform_gc_aux(uint32_t env, uint32_t env2, uint32_t exp, uint32_t exp2, uint32_t *aux_data, uint32_t aux_size);
 
 
 // Array functionality
-extern int heap_allocate_array(uint32_t *res, uint32_t size, uint32_t type); 
+extern int heap_allocate_array(uint32_t *res, uint32_t size, uint32_t type);
 
 #endif
--- src/heap.c
+++ src/heap.c
@@ -22,19 +22,19 @@
 #include "symrepr.h"
 #ifdef VISUALIZE_HEAP
 #include "heap_vis.h"
-#endif 
+#endif
 
 
 uint32_t global_env;
 
-static cons_t*      heap = NULL; 
-static uint32_t     heap_base; 
+static cons_t*      heap = NULL;
+static uint32_t     heap_base;
 static heap_state_t heap_state;
 
-static uint32_t     SYMBOL_NIL; 
+static uint32_t     SYMBOL_NIL;
 
 // ref_cell: returns a reference to the cell addressed by bits 3 - 26
-//           Assumes user has checked that IS_PTR was set 
+//           Assumes user has checked that IS_PTR was set
 cons_t* ref_cell(uint32_t addr) {
   return (cons_t*)(heap_base + (addr & PTR_VAL_MASK));
 }
@@ -57,7 +57,7 @@ static void set_cdr_(cons_t *cell, uint32_t v) {
 
 static void set_gc_mark(cons_t *cell) {
   uint32_t cdr = read_cdr(cell);
-  set_cdr_(cell, cdr | GC_MARKED); 
+  set_cdr_(cell, cdr | GC_MARKED);
 }
 
 static void clr_gc_mark(cons_t *cell) {
@@ -71,94 +71,94 @@ static uint32_t get_gc_mark(cons_t* cell) {
 }
 
 int generate_freelist(size_t num_cells) {
-  size_t i = 0; 
-  
+  size_t i = 0;
+
   if (!heap) return 0;
-  
-  heap_state.freelist = ENC_CONS_PTR(0); 
+
+  heap_state.freelist = ENC_CONS_PTR(0);
 
   // Add all cells to free list
   for (i = 1; i < num_cells; i ++) {
-    cons_t *t = ref_cell( ENC_CONS_PTR(i-1)); 
-    set_car_(t, ENC_SYM(SYMBOL_NIL));    // all cars in free list are nil 
-    set_cdr_(t, ENC_CONS_PTR(i)); 
+    cons_t *t = ref_cell( ENC_CONS_PTR(i-1));
+    set_car_(t, ENC_SYM(SYMBOL_NIL));    // all cars in free list are nil
+    set_cdr_(t, ENC_CONS_PTR(i));
   }
-  
+
   heap_state.freelist_last = ENC_CONS_PTR(num_cells-1);
   set_cdr_(ref_cell(heap_state.freelist_last), ENC_SYM(SYMBOL_NIL));
 
   if (read_cdr(ref_cell(heap_state.freelist_last)) == ENC_SYM(SYMBOL_NIL)) {
     return 1;
   }
-  return 0; 
+  return 0;
 }
 
 int heap_init(size_t num_cells) {
 
   // retrieve nil symbol value f
-  SYMBOL_NIL = symrepr_nil(); 
+  SYMBOL_NIL = symrepr_nil();
 
-  // Allocate heap 
+  // Allocate heap
   heap = (cons_t *)malloc(num_cells * sizeof(cons_t));
 
   if (!heap) return 0;
 
   heap_base = (uint32_t)heap;
-  
+
   // Initialize heap statistics
   heap_state.heap_base    = heap_base;
   heap_state.heap_bytes   = (uint32_t)(num_cells * sizeof(cons_t));
   heap_state.heap_size    = num_cells;
-  
+
   heap_state.num_alloc           = 0;
-  heap_state.num_alloc_arrays    = 0; 
+  heap_state.num_alloc_arrays    = 0;
   heap_state.gc_num              = 0;
   heap_state.gc_marked           = 0;
   heap_state.gc_recovered        = 0;
-  heap_state.gc_recovered_arrays = 0; 
-  
-  return (generate_freelist(num_cells)); 
+  heap_state.gc_recovered_arrays = 0;
+
+  return (generate_freelist(num_cells));
 }
 
 void heap_del(void) {
   if (heap)
-    free(heap); 
+    free(heap);
 }
 
 uint32_t heap_num_free(void) {
 
   uint32_t count = 0;
-  uint32_t curr = heap_state.freelist; 
-  
+  uint32_t curr = heap_state.freelist;
+
   while (TYPE_OF(curr) == PTR_TYPE_CONS) {
     curr = read_cdr(ref_cell(curr));
-    count++; 
+    count++;
   }
   // Prudence.
   if (!(TYPE_OF(curr) == VAL_TYPE_SYMBOL) &&
-      (DEC_SYM(curr) == SYMBOL_NIL)){ 
-    return 0; 
-  } 
-  return count; 
+      (DEC_SYM(curr) == SYMBOL_NIL)){
+    return 0;
+  }
+  return count;
 }
 
 
 uint32_t heap_allocate_cell(uint32_t ptr_type) {
 
   uint32_t res;
-  
+
   if (! IS_PTR(heap_state.freelist)) {
     // Free list not a ptr (should be Symbol NIL)
     if ((VAL_TYPE(heap_state.freelist) == VAL_TYPE_SYMBOL) &&
 	(DEC_SYM(heap_state.freelist) == SYMBOL_NIL)) {
       // all is as it should be (but no free cells)
-      return heap_state.freelist; 
+      return heap_state.freelist;
     } else {
       // something is most likely very wrong
-      //printf("heap_allocate_cell Error\n"); 
+      //printf("heap_allocate_cell Error\n");
       return ENC_SYM(SYMBOL_NIL);
-    }   
-  } else { // it is a ptr replace freelist with cdr of freelist; 
+    }
+  } else { // it is a ptr replace freelist with cdr of freelist;
     res = heap_state.freelist;
     res = res | ptr_type;
     heap_state.freelist =
@@ -169,8 +169,8 @@ uint32_t heap_allocate_cell(uint32_t ptr_type) {
 
   // set some ok initial values (nil . nil)
   set_car_(ref_cell(res), ENC_SYM(SYMBOL_NIL));
-  set_cdr_(ref_cell(res), ENC_SYM(SYMBOL_NIL)); 	  
-  
+  set_cdr_(ref_cell(res), ENC_SYM(SYMBOL_NIL));
+
   // clear GC bit on allocated cell
   clr_gc_mark(ref_cell(res));
   return res;
@@ -180,13 +180,13 @@ uint32_t heap_num_allocated(void) {
   return heap_state.num_alloc;
 }
 uint32_t heap_size() {
-  return heap_state.heap_size; 
+  return heap_state.heap_size;
 }
 
 uint32_t heap_size_bytes(void) {
   return heap_state.heap_bytes;
 }
-  
+
 void heap_get_state(heap_state_t *res) {
   res->heap_base           = heap_state.heap_base;
   res->freelist            = heap_state.freelist;
@@ -205,39 +205,39 @@ void heap_get_state(heap_state_t *res) {
 int gc_mark_phase(uint32_t env) {
 
   if (!IS_PTR(env)) {
-      return 1; // Nothing to mark here 
+      return 1; // Nothing to mark here
   }
 
   if (get_gc_mark(ref_cell(env))) {
     return 1; // Circular object on heap, or visited (aux)..
   }
-    
+
   // There is at least a pointer to one cell here. Mark it and recurse over  car and cdr 
   heap_state.gc_marked ++;
 
-  set_gc_mark(ref_cell(env)); 
+  set_gc_mark(ref_cell(env));
 
   int res = 1;
-  if (IS_PTR(car(env))) 
+  if (IS_PTR(car(env)))
     res &= gc_mark_phase(car(env));
   if (IS_PTR(cdr(env)))
-    res &= gc_mark_phase(cdr(env)); 
-  
-  return res; 
+    res &= gc_mark_phase(cdr(env));
+
+  return res;
 }
 
 // The free list should be a "proper list"
-// Using a while loop to traverse over the cdrs 
+// Using a while loop to traverse over the cdrs
 int gc_mark_freelist() {
 
   uint32_t curr;
   cons_t *t;
   uint32_t fl = heap_state.freelist;
 
-  if (!IS_PTR(fl)) { 
+  if (!IS_PTR(fl)) {
     if ((VAL_TYPE(fl) == VAL_TYPE_SYMBOL) &&
 	(DEC_SYM(fl) == SYMBOL_NIL)){
-      return 1; // Nothing to mark here 
+      return 1; // Nothing to mark here
     } else {
       printf(" ERROR CASE! %x \n", fl);
       return 0;
@@ -262,7 +262,7 @@ int gc_mark_freelist() {
 int gc_mark_aux(uint32_t *aux_data, uint32_t aux_size) {
 
   cons_t *t;
- 
+
   for (int i = 0; i < aux_size; i ++) {
     if (IS_PTR(aux_data[i])) {
 
@@ -277,40 +277,42 @@ int gc_mark_aux(uint32_t *aux_data, uint32_t aux_size) {
 	   pt_t == PTR_TYPE_REF ||
 	   pt_t == PTR_TYPE_STREAM &&
 	   pt_v < heap_state.heap_size) {
- 
+
 	gc_mark_phase(aux_data[i]);
       }
     }
   }
-  
-  return 1; 
+
+  return 1;
 }
 
 
 // Sweep moves non-marked heap objects to the free list.
 int gc_sweep_phase(void) {
 
-  uint32_t i = 0; 
+  uint32_t i = 0;
   cons_t *heap = (cons_t *)heap_base;
-  cons_t *fl_last; 
-  
+  cons_t *fl_last;
+
   uint32_t cdr;
-  
-  for (i = 0; i < heap_state.heap_size; i ++) {   
+
+  for (i = 0; i < heap_state.heap_size; i ++) {
     if ( !get_gc_mark(&heap[i])){
       fl_last = ref_cell(heap_state.freelist_last);
 
       if ( DEC_SYM(read_cdr(fl_last)) != SYMBOL_NIL) {
 	printf( "ERROR: cdr of fl_last not nil\n");
       }
-      
-      // Clear the "freed" cell. 
-      set_cdr_(&heap[i], 0); 
+
+
+
+      // Clear the "freed" cell.
+      set_cdr_(&heap[i], 0);
       set_cdr_(&heap[i], ENC_SYM(SYMBOL_NIL));
       set_car_(&heap[i], ENC_SYM(SYMBOL_NIL));
 
       // create pointer to free cell to put at end of freelist
-      uint32_t addr = ENC_CONS_PTR(i); 
+      uint32_t addr = ENC_CONS_PTR(i);
 
       set_cdr_(fl_last, addr);
       set_gc_mark(fl_last); // the above set_cdr_ clears the gc mark on fl_last.
@@ -321,13 +323,13 @@ int gc_sweep_phase(void) {
     }
     clr_gc_mark(&heap[i]);
   }
-  return 1; 
+  return 1;
 }
 
 int heap_perform_gc(uint32_t env) {
   heap_state.gc_num ++;
-  heap_state.gc_recovered = 0; 
-  heap_state.gc_marked = 0; 
+  heap_state.gc_recovered = 0;
+  heap_state.gc_marked = 0;
 
   gc_mark_freelist();
   gc_mark_phase(env);
@@ -336,20 +338,20 @@ int heap_perform_gc(uint32_t env) {
 
 int heap_perform_gc_aux(uint32_t env, uint32_t env2, uint32_t exp, uint32_t exp2, uint32_t *aux_data, uint32_t aux_size) {
   heap_state.gc_num ++;
-  heap_state.gc_recovered = 0; 
-  heap_state.gc_marked = 0; 
+  heap_state.gc_recovered = 0;
+  heap_state.gc_marked = 0;
 
   gc_mark_freelist();
   gc_mark_phase(exp);
   gc_mark_phase(exp2);
   gc_mark_phase(env);
   gc_mark_phase(env2);
-  gc_mark_aux(aux_data, aux_size);   
+  gc_mark_aux(aux_data, aux_size);
 
 #ifdef VISUALIZE_HEAP
   heap_vis_gen_image();
-#endif 
-  
+#endif
+
   return gc_sweep_phase();
 }
 
@@ -366,33 +368,33 @@ uint32_t cons(uint32_t car, uint32_t cdr) {
 }
 
 uint32_t car(uint32_t c){
-  
+
   if (TYPE_OF(c) == VAL_TYPE_SYMBOL &&
       DEC_SYM(c) == SYMBOL_NIL) {
     return c; // if nil, return nil.
   }
-  
+
   //if (TYPE_OF(c) == PTR_TYPE_CONS ||
   //    TYPE_OF(c) == PTR_TYPE_F32) {   //TODO: one test should be enough
-  if (IS_PTR(c) ){ 
+  if (IS_PTR(c) ){
     cons_t *cell = ref_cell(c);
     return read_car(cell);
-  } 
-  return ENC_SYM(symrepr_terror()); 
+  }
+  return ENC_SYM(symrepr_terror());
 }
 
 uint32_t cdr(uint32_t c){
- 
+
   if (TYPE_OF(c) == VAL_TYPE_SYMBOL &&
       DEC_SYM(c) == SYMBOL_NIL) {
     return c; // if nil, return nil.
   }
-  
+
   if (TYPE_OF(c) == PTR_TYPE_CONS) {
     cons_t *cell = ref_cell(c);
     return read_cdr(cell);
   }
-  return ENC_SYM(symrepr_terror()); 
+  return ENC_SYM(symrepr_terror());
 }
 
 void set_car(uint32_t c, uint32_t v) {
@@ -405,33 +407,33 @@ void set_car(uint32_t c, uint32_t v) {
 void set_cdr(uint32_t c, uint32_t v) {
   if (TYPE_OF(c) == PTR_TYPE_CONS){
     cons_t *cell = ref_cell(c);
-    set_cdr_(cell,v); 
+    set_cdr_(cell,v);
   }
 }
 
-/* calculate length of a proper list */ 
+/* calculate length of a proper list */
 uint32_t length(uint32_t c) {
   uint32_t len = 0;
-  
+
   while (TYPE_OF(c) == PTR_TYPE_CONS){
-    len ++; 
-    c = cdr(c); 
+    len ++;
+    c = cdr(c);
   }
-  return len; 
+  return len;
 }
 
-/* reverse a proper list */ 
+/* reverse a proper list */
 uint32_t reverse(uint32_t list) {
 
-  uint32_t curr = list; 
-  
-  uint32_t new_list = ENC_SYM(symrepr_nil()); 
+  uint32_t curr = list;
+
+  uint32_t new_list = ENC_SYM(symrepr_nil());
   while (TYPE_OF(curr) == PTR_TYPE_CONS) {
 
     new_list = cons(car(curr), new_list);
-    curr = cdr(curr); 
+    curr = cdr(curr);
   }
-  return new_list; 
+  return new_list;
 }
 
 
@@ -443,7 +445,7 @@ uint32_t reverse(uint32_t list) {
 
 // Arrays are part of the heap module because their lifespan is managed
 // by the garbage collector. The data in the array is not stored
-// in the "heap of cons cells". 
+// in the "heap of cons cells".
 int heap_allocate_array(uint32_t *res, uint32_t size, uint32_t type){
 
   array_t *array = malloc(sizeof(array_t));
@@ -453,19 +455,19 @@ int heap_allocate_array(uint32_t *res, uint32_t size, uint32_t type){
   switch(type) {
   case PTR_TYPE_I32: // array of I32
     array->data.i32 = (int32_t*)malloc(size * (sizeof(int32_t)));
-    if (array->data.i32 == NULL) return 0; 
+    if (array->data.i32 == NULL) return 0;
     break;
   case PTR_TYPE_U32: // array of U32
     array->data.u32 = (uint32_t*)malloc(size * (sizeof(uint32_t)));
-    if (array->data.u32 == NULL) return 0; 
+    if (array->data.u32 == NULL) return 0;
     break;
   case PTR_TYPE_F32: // array of Float
     array->data.f = (float*)malloc(size * (sizeof(float)));
-    if (array->data.f == NULL) return 0; 
+    if (array->data.f == NULL) return 0;
     break;
   case VAL_TYPE_CHAR: // Array of Char
     array->data.c = (char*)malloc(size * (sizeof(char)));
-    if (array->data.c == NULL) return 0; 
+    if (array->data.c == NULL) return 0;
     break;
   case VAL_TYPE_I28:
     array->data.i32 = (int32_t*)malloc(size * (sizeof(int32_t)));
@@ -480,19 +482,19 @@ int heap_allocate_array(uint32_t *res, uint32_t size, uint32_t type){
     *res = ENC_SYM(symrepr_nil());
     return 0;
   }
-  
+
   array->aux = 0;
   array->elt_type = type;
   array->size = size;
-  
+
   set_car(cell, (uint32_t)array);
   set_cdr(cell, ENC_SYM(symrepr_nil()));
-	   
+
   cell = cell | PTR_TYPE_ARRAY;
 
   *res = cell;
 
-  heap_state.num_alloc_arrays ++; 
-  
-  return 1; 
+  heap_state.num_alloc_arrays ++;
+
+  return 1;
 }
