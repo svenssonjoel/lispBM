9c67c1ca2285260c7d205ad5de7eb01e43c393ee
Joel Svensson <svenssonjoel@yahoo.se>
2018-12-29 14:13:50 +0100

    cleaning

--- src/symrepr.c
+++ src/symrepr.c
@@ -22,31 +22,29 @@
 
 #include "symrepr.h"
 
-/* 
-   Name -> 28bit integer mapping that is (I hope) somewhat 
-   efficient in both directions. (it's a shot from the hip... ) 
+/*
+   Name -> 28bit integer mapping that is (I hope) somewhat
+   efficient in both directions. (it's a shot from the hip... )
 
-   In the best case, looking up the 28bit id has cost relative to 
-   length of name. In the worst, it has that cost + cost of a walk 
-   over linked list (that can at most be 4095 links long). 
+   In the best case, looking up the 28bit id has cost relative to
+   length of name. In the worst, it has that cost + cost of a walk
+   over linked list (that can at most be 4095 links long).
 
    In the best case, looking up a name given a 28bit id has constant cost.
-   In the worst case it has the same linked list traversal. 
-   
-   Replace with something more thought through (or looked up) later. 
+   In the worst case it has the same linked list traversal.
+
+   Replace with something more thought through (or looked up) later.
 
      - There is a 16 bit hash table (only 49999 buckets targeted by hash function) 
      - and 12 additional bits (bucket depth 4096)
      - The buckets numbered 49999 - 65534 are used by gensyming new symbols.
-     - The last bucket 65535 is used for internal implementation needs. 
-       There are 4096 symbols that cannot collide with any user specified 
-       or gensymed symbol. 
-     
-     
+     - The last bucket 65535 is used for internal implementation needs.
+       There are 4096 symbols that cannot collide with any user specified
+       or gensymed symbol.
  */
 
 #define HASHTAB_MALLOC_SIZE 65535 // 65535
-#define HASHTAB_SIZE 49999 // 65521    
+#define HASHTAB_SIZE 49999 // 65521
 #define BUCKET_DEPTH 4096
 #define SMALL_PRIMES 11
 
@@ -64,23 +62,21 @@
 #define DEF_REPR_MERROR    11
 #define DEF_REPR_DEFINE    12
 
-#define GENSYM_FLAG        0xFFFF
-#define IS_GENSYM(X)       (((X) & 0xFFFF) == 0xFFFF)
-#define GENSYM_SHIFT       16
+// Special symbol ids
+#define SPECIAL_SYM_ARRAY 0x0001FFFF
 
+static uint32_t gensym_next = HASHTAB_SIZE;
 
-static uint32_t gensym_next = HASHTAB_SIZE;  
+static uint32_t hash_string(char *str);
 
-static uint32_t hash_string(char *str); 
-
-static uint32_t def_repr[13]; 
+static uint32_t def_repr[13];
 
 typedef struct s_name_mapping {
   uint32_t key; // hash including collision id //
   char* name;
   struct s_name_mapping* next;
-} name_mapping_t; 
-  
+} name_mapping_t;
+
 name_mapping_t **name_table = NULL;
 
 int add_default_symbols(void) {
@@ -93,13 +89,13 @@ int add_default_symbols(void) {
   res &= symrepr_addsym("if"     , &def_repr[DEF_REPR_IF]);
   res &= symrepr_addsym("lambda" , &def_repr[DEF_REPR_LAMBDA]);
   res &= symrepr_addsym("closure", &def_repr[DEF_REPR_CLOSURE]);
-  res &= symrepr_addsym("let"    , &def_repr[DEF_REPR_LET]); 
+  res &= symrepr_addsym("let"    , &def_repr[DEF_REPR_LET]);
   res &= symrepr_addsym("read_error" , &def_repr[DEF_REPR_RERROR]);
   res &= symrepr_addsym("type_error" , &def_repr[DEF_REPR_TERROR]);
   res &= symrepr_addsym("eval_error" , &def_repr[DEF_REPR_EERROR]);
-  res &= symrepr_addsym("out_of_memory_error" , &def_repr[DEF_REPR_MERROR]); 
-  res &= symrepr_addsym("define" , &def_repr[DEF_REPR_DEFINE]); 
-  
+  res &= symrepr_addsym("out_of_memory_error" , &def_repr[DEF_REPR_MERROR]);
+  res &= symrepr_addsym("define" , &def_repr[DEF_REPR_DEFINE]);
+
   return res;
 }
 
@@ -119,7 +115,7 @@ uint32_t symrepr_define(void)  { return def_repr[DEF_REPR_DEFINE]; }
 
 int symrepr_init(void) {
   name_table = (name_mapping_t**)malloc(HASHTAB_MALLOC_SIZE * sizeof(name_mapping_t*));
-  if (!name_table) return 0; 
+  if (!name_table) return 0;
   memset(name_table, 0, HASHTAB_MALLOC_SIZE * sizeof(name_mapping_t*));
   return add_default_symbols();
 }
@@ -127,7 +123,7 @@ int symrepr_init(void) {
 int gensym(uint32_t *res) {
 
   uint32_t v = gensym_next;
-  int n; 
+  int n;
   char gensym_name[1024];
   memset(gensym_name,0,1024);
 
@@ -145,12 +141,12 @@ int gensym(uint32_t *res) {
     name_mapping_t *head = name_table[v];
     uint32_t hkey_id = head->key & 0xFFFF0000 ;
 
-    // If new ID would be too big return failure 
+    // If new ID would be too big return failure
     if ((hkey_id >> 16) >= (BUCKET_DEPTH - 1)) {
       return 0;
     }
 
-    /* problem if hkey_id = 0xFFFF0000 */ 
+    /* problem if hkey_id = 0xFFFF0000 */
     name_table[v] = (name_mapping_t*)malloc(sizeof(name_mapping_t)); //replace
     name_table[v]->key = v + (hkey_id + (1 << 16));
     uint32_t v_prim = v + (hkey_id + (1 << 16));
@@ -164,46 +160,46 @@ int gensym(uint32_t *res) {
 
   gensym_next++;
   if (gensym_next >= (HASHTAB_MALLOC_SIZE-1)) gensym_next = HASHTAB_SIZE;
-  return 1; 
+  return 1;
 }
 
 int symrepr_addsym(char *name, uint32_t* id) {
   size_t   n = 0;
 
   if(strlen(name) == 0) return 0; //return failure if empty symbol
-  
+
   uint32_t hash = hash_string(name);
-  
+
   if (hash >= HASHTAB_SIZE) /* impossible */ return 0;
 
   if (name_table[hash] == NULL){
     name_table[hash] = (name_mapping_t*)malloc(sizeof(name_mapping_t));
     name_table[hash]->key = hash;
-    if (id != NULL) *id = hash; 
+    if (id != NULL) *id = hash;
     n = strlen(name) + 1;
     name_table[hash]->name = (char*)malloc(n);
     strncpy(name_table[hash]->name, name, n);
-    name_table[hash]->next = NULL; 
+    name_table[hash]->next = NULL;
   } else {
-    uint32_t t_id; 
+    uint32_t t_id;
     if (symrepr_lookup(name, &t_id)) {
       /* name already in table */
 
-      if (id != NULL) *id = t_id; 
-      
-      return 0; /* set id, but return failure */ 
+      if (id != NULL) *id = t_id;
+
+      return 0; /* set id, but return failure */
     }
-    
+
     /* collision */
     name_mapping_t *head = name_table[hash];
     uint32_t hkey_id = head->key & 0xFFFF0000 ;
 
-    // If new ID would be too big return failure 
+    // If new ID would be too big return failure
     if ((hkey_id >> 16) >= (BUCKET_DEPTH - 1)) {
       return 0;
     }
-    
-    /* problem if hkey_id = 0xFFFF0000 */ 
+
+    /* problem if hkey_id = 0xFFFF0000 */
     name_table[hash] = (name_mapping_t*)malloc(sizeof(name_mapping_t));
     name_table[hash]->key = hash + (hkey_id + (1 << 16));
     if (id != NULL) *id = hash + (hkey_id + (1 << 16));
@@ -212,30 +208,34 @@ int symrepr_addsym(char *name, uint32_t* id) {
     strncpy(name_table[hash]->name, name, n);
     name_table[hash]->next = head;
   }
-  return 1; 
+  return 1;
 }
 
 int symrepr_lookup(char *name, uint32_t* id) {
 
-  int r = 0; 
+  int r = 0;
   uint32_t hash = hash_string(name);
-  
+
   if (name_table[hash] == NULL) return 0;
 
   name_mapping_t *head = name_table[hash];
- 
+
   while (head) {
     if (strcmp(head->name, name) == 0) {
       r = 1;
-      *id = head->key; 
+      *id = head->key;
     }
-    head = head->next; 
+    head = head->next;
   }
-  return r; 
+  return r;
 }
 
 char *symrepr_lookup_name(uint32_t id) {
+
+
   uint32_t hash = id & (uint32_t)0x0000FFFF; /*extract index*/
+  if(hash == 65535) return "special_symbol";
+
   if (name_table[hash]) {
     name_mapping_t *head = name_table[hash];
 
@@ -243,10 +243,10 @@ char *symrepr_lookup_name(uint32_t id) {
       if (head->key == id) {
 	return head->name;
       }
-      head = head->next; 
+      head = head->next;
     }
   }
-  return NULL; 
+  return NULL;
 }
 
 void symrepr_print(void) {
@@ -254,10 +254,10 @@ void symrepr_print(void) {
 
   for (i = 0; i < HASHTAB_MALLOC_SIZE; i ++) {
     if (name_table[i] != NULL) {
-      name_mapping_t *head = name_table[i]; 
+      name_mapping_t *head = name_table[i];
       while (head) {
-	printf("%d : %x : %s\n", head->key, head->key, head->name); 
-	head = head->next; 
+	printf("%d : %x : %s\n", head->key, head->key, head->name);
+	head = head->next;
       }
     }
   }
@@ -266,38 +266,36 @@ void symrepr_print(void) {
 void symrepr_del(void) {
   int i;
 
-  if(!name_table) return; 
-  
+  if(!name_table) return;
+
   for (i = 0; i < HASHTAB_MALLOC_SIZE; i ++) {
     if (name_table[i]) {
       name_mapping_t *head = name_table[i];
-      name_mapping_t *next; 
+      name_mapping_t *next;
       while (head) {
 	next = head->next;
-	free(head->name); 
+	free(head->name);
 	free(head);
-	head = next; 
+	head = next;
       }
-    } 
+    }
   }
 }
 
 uint32_t small_primes[SMALL_PRIMES] =
-  /* {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31}; */ 
+  /* {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31}; */
   /* {37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79}; */
-  {359, 419, 463, 523, 593, 643, 701, 761, 827, 883, 953}; 
+  {359, 419, 463, 523, 593, 643, 701, 761, 827, 883, 953};
 uint32_t hash_string(char *str) {
 
   uint32_t r = 1;
-  size_t n = strlen(str); 
-  
+  size_t n = strlen(str);
+
   for (int i = 0; i < n; i ++) {
-    uint32_t sp = small_primes[i % SMALL_PRIMES]; 
-    r = (r + (sp * str[i])) % HASHTAB_SIZE; 
+    uint32_t sp = small_primes[i % SMALL_PRIMES];
+    r = (r + (sp * str[i])) % HASHTAB_SIZE;
   }
 
-  return r; 
+  return r;
 }
-
-
 
