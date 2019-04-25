b92ccdac6cb5c5f35d1e732a87112a18da068843
Joel Svensson <svenssonjoel@yahoo.se>
2019-04-25 17:34:28 +0200

    cleanup

--- tests/test_lisp_code2.c
+++ tests/test_lisp_code2.c
@@ -18,7 +18,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include "heap.h" 
+#include "heap.h"
 #include "symrepr.h"
 #include "builtin.h"
 #include "eval.h"
@@ -27,21 +27,21 @@
 
 int main(int argc, char **argv) {
 
-  int res = 0; 
+  int res = 0;
 
   if (argc < 2) {
-    printf("Incorrect arguments\n"); 
+    printf("Incorrect arguments\n");
     return 0;
   }
-  
+
   FILE* fp = fopen(argv[1], "r");
 
   if (fp == NULL) {
-    printf("Error opening file\n"); 
-    return 0; 
+    printf("Error opening file\n");
+    return 0;
   }
 
-  fseek(fp, 0, SEEK_END); 
+  fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
   if (size <= 0) {
     printf("Error file empty %s\n", argv[1]);
@@ -54,15 +54,15 @@ int main(int argc, char **argv) {
     printf("Error empty file?\n");
     return 0;
   }
-  
+
   res = symrepr_init();
-  if (res) 
+  if (res)
     printf("Symrepr initialized.\n");
   else {
     printf("Error initializing symrepr!\n");
     return 0;
   }
-  
+
   unsigned int heap_size = 8 * 1024 * 1024;
   res = heap_init(heap_size);
   if (res)
@@ -86,31 +86,31 @@ int main(int argc, char **argv) {
   else {
     printf("Error initializing evaluator.\n");
   }
-  
+
   VALUE t;
   printf("Parsing\n");
   t = tokpar_parse(code_buffer);
   printf("Done!\n");
-  
-  printf("I: "); simple_print(t); printf("\n"); 
-  
+
+  printf("I: "); simple_print(t); printf("\n");
+
   t = eval_program(t);
-  
+
   printf("O: "); simple_print(t); printf("\n");
 
   if ( dec_sym(t) == symrepr_eerror()) {
     res = 0;
-  }  
-  
+  }
+
   if (res && type_of(t) == VAL_TYPE_SYMBOL && dec_sym(t) == symrepr_true()) { // structural_equality(car(rest),car(cdr(rest)))) {
-    printf("Test: OK!\n"); 
+    printf("Test: OK!\n");
     res = 1;
   } else {
-    printf("Test: Failed!\n"); 
-    res = 0; 
-  }  
+    printf("Test: Failed!\n");
+    res = 0;
+  }
   symrepr_del();
   heap_del();
-  
-  return res;  
+
+  return res;
 }
