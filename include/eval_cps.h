9298e8673f96637d43680012c4271ccd7f43ebef
Joel Svensson <svenssonjoel@yahoo.se>
2019-05-05 11:05:21 +0200

    removing eval_cps_es.c. Will incrementally apply imrpovements to eval_cps instead

--- include/eval_cps.h
+++ include/eval_cps.h
@@ -1,8 +1,23 @@
+/*
+    Copyright 2018 Joel Svensson	svenssonjoel@yahoo.se
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef EVAL_CPS_H_
 #define EVAL_CPS_H_
 
 extern uint32_t eval_cps_get_env(void);
-
 extern int eval_cps_init(void);
 extern uint32_t eval_cps_program(uint32_t lisp);
 #endif
