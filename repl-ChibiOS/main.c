293a216cbda6d3c0f4cefa5640a0df42d5712984
Joel Svensson <svenssonjoel@yahoo.se>
2020-02-07 13:54:12 +0100

    update repl-chibios

--- repl-ChibiOS/main.c
+++ repl-ChibiOS/main.c
@@ -33,19 +33,13 @@
     limitations under the License.
  */
 
-#include <stdio.h>
-#include <string.h>
-#include <string.h>
-#include <stdlib.h>
-#include <ctype.h>
+#include "ctype.h"
 
 #include "ch.h"
 #include "hal.h"
 
-#include "shell.h"
-#include "chprintf.h"
-
 #include "usbcfg.h"
+#include "chprintf.h"
 
 #include "heap.h"
 #include "symrepr.h"
@@ -53,37 +47,23 @@
 #include "print.h"
 #include "tokpar.h"
 #include "prelude.h"
+#include "extensions.h"
 
-
-/*===========================================================================*/
-/* Command line related.                                                     */
-/*===========================================================================*/
-
-#define SHELL_WA_SIZE   THD_WORKING_AREA_SIZE(2048)
-
-unsigned char inbyte(BaseSequentialStream *chp) {
-  unsigned char c;
-  while (streamRead(chp, &c, 1) == 0);
-  return c;
-}
-
-void outbyte(BaseSequentialStream *chp, char c) {
-  streamPut(chp, c);
-}
+BaseSequentialStream *chp = NULL;
 
 int inputline(BaseSequentialStream *chp, char *buffer, int size) {
   int n = 0;
   unsigned char c;
   for (n = 0; n < size - 1; n++) {
 
-    c = inbyte(chp);
+    c = streamGet(chp);
     switch (c) {
     case 127: /* fall through to below */
     case '\b': /* backspace character received */
       if (n > 0)
         n--;
       buffer[n] = 0;
-      outbyte(chp,'\b'); /* output backspace character */
+      streamPut(chp,0x8); /* output backspace character */
       n--; /* set up next iteration to deal with preceding char location */
       break;
     case '\n': /* fall through to \r */
@@ -92,7 +72,7 @@ int inputline(BaseSequentialStream *chp, char *buffer, int size) {
       return n;
     default:
       if (isprint(c)) { /* ignore non-printable characters */
-        outbyte(chp,c);
+        streamPut(chp,c);
         buffer[n] = c;
       } else {
         n -= 1;
@@ -104,45 +84,94 @@ int inputline(BaseSequentialStream *chp, char *buffer, int size) {
   return 0; // Filled up buffer without reading a linebreak
 }
 
-static void cmd_repl(BaseSequentialStream *chp, int argc, char *argv[]) {
+#define REPL_WA_SIZE THD_WORKING_AREA_SIZE(10*4096)
 
-  (void) argc;
-  (void) argv;
+VALUE ext_print(VALUE *args, int argn) {
+  if (argn < 1) return enc_sym(symrepr_nil());
 
-  size_t len = 1024;
-  char *str = malloc(1024);
-  char *outbuf = malloc(1024);
+  for (int i = 0; i < argn; i ++) {
+    VALUE t = args[i];
+
+    if (is_ptr(t) && ptr_type(t) == PTR_TYPE_ARRAY) {
+      array_t *array = (array_t *)car(t);
+      switch (array->elt_type){
+      case VAL_TYPE_CHAR:
+	chprintf(chp,"%s", array->data.c);
+	break;
+      default:
+	return enc_sym(symrepr_nil());
+	break;
+      }
+    } else if (val_type(t) == VAL_TYPE_CHAR) {
+      chprintf(chp,"%c", dec_char(t));
+    } else {
+      return enc_sym(symrepr_nil());
+    }
+ 
+  }
+  return enc_sym(symrepr_true());
+}
+
+
+
+int reset_repl(int heap_size) {
+  symrepr_del();
+  heap_del();
+
   int res = 0;
-
-  heap_state_t heap_state;
 
   res = symrepr_init();
   if (res)
     chprintf(chp,"Symrepr initialized.\n\r");
   else {
     chprintf(chp,"Error initializing symrepr!\n\r");
-    return;
+    return res;
   }
-  int heap_size = 2048;
+  
   res = heap_init(heap_size);
   if (res)
     chprintf(chp,"Heap initialized. Free cons cells: %u\n\r", heap_num_free());
   else {
     chprintf(chp,"Error initializing heap!\n\r");
-    return;
+    return res;
   }
 
-  res = eval_cps_init(true);
+  res = eval_cps_init(false);
   if (res)
     chprintf(chp,"Evaluator initialized.\n\r");
   else {
     chprintf(chp,"Error initializing evaluator.\n\r");
+    return res;
   }
+  
+  res = extensions_add("print", ext_print);
+  if (res)
+    printf("Extension added.\n");
+  else
+    printf("Error adding extension.\n");
 
   VALUE prelude = prelude_load();
   eval_cps_program(prelude);
 
   chprintf(chp,"Lisp REPL started (ChibiOS)!\n\r");
+  
+  return res;
+}
+
+
+static THD_FUNCTION(repl, arg) {
+
+  (void) arg;
+  
+  size_t len = 1024;
+  char *str = malloc(1024);
+  char *outbuf = malloc(2048);
+
+  heap_state_t heap_state;
+
+  int heap_size = 2048;
+
+  reset_repl(heap_size);
 
   while (1) {
     chprintf(chp,"# ");
@@ -151,17 +180,20 @@ static void cmd_repl(BaseSequentialStream *chp, int argc, char *argv[]) {
     inputline(chp,str, len);
     chprintf(chp,"\n\r");
 
-    if (strncmp(str, ":info", 5) == 0) {
+    if (strncmp(str, ":reset", 6) == 0) {
+      reset_repl(heap_size);
+      continue;
+    } else if (strncmp(str, ":info", 5) == 0) {
       chprintf(chp,"##(ChibiOS)#################################################\n\r");
       chprintf(chp,"Used cons cells: %lu \n\r", heap_size - heap_num_free());
-      chprintf(chp,"ENV: "); simple_snprint(outbuf,1023, eval_cps_get_env()); chprintf(chp, "%s \n\r", outbuf);
+      chprintf(chp,"ENV: "); simple_snprint(outbuf,2048, eval_cps_get_env()); chprintf(chp, "%s \n\r", outbuf);
       heap_get_state(&heap_state);
       chprintf(chp,"GC counter: %lu\n\r", heap_state.gc_num);
       chprintf(chp,"Recovered: %lu\n\r", heap_state.gc_recovered);
       chprintf(chp,"Marked: %lu\n\r", heap_state.gc_marked);
       chprintf(chp,"Free cons cells: %lu\n\r", heap_num_free());
       chprintf(chp,"############################################################\n\r");
-      memset(outbuf,0, 1024);
+      memset(outbuf,0, 2048);
     } else if (strncmp(str, ":quit", 5) == 0) {
       break;
     } else {
@@ -183,16 +215,6 @@ static void cmd_repl(BaseSequentialStream *chp, int argc, char *argv[]) {
   heap_del();
 }
 
-static const ShellCommand commands[] = {
-		{"repl", cmd_repl},
-		{NULL, NULL}
-};
-
-static const ShellConfig shell_cfg1 = {
-		(BaseSequentialStream *)&SDU1,
-		commands
-};
-
 int main(void) {
 	halInit();
 	chSysInit();
@@ -208,20 +230,18 @@ int main(void) {
 	usbDisconnectBus(serusbcfg.usbp);
 	chThdSleepMilliseconds(1500);
 	usbStart(serusbcfg.usbp, &usbcfg);
-	usbConnectBus(serusbcfg.usbp);
+	usbConnectBus(serusbcfg.usbp);	
 
-	shellInit();
+	chp = (BaseSequentialStream*)&SDU1;
+	
+	chThdCreateFromHeap(NULL, REPL_WA_SIZE,
+			    "repl", NORMALPRIO + 1,
+			    repl, (void *)NULL);
+	
 
-	/*
-	 * Normal main() thread activity, spawning shells.
-	 */
-	while (true) {
-		if (SDU1.config->usbp->state == USB_ACTIVE) {
-			thread_t *shelltp = chThdCreateFromHeap(NULL, SHELL_WA_SIZE,
-					"shell", NORMALPRIO + 1,
-					shellThread, (void *)&shell_cfg1);
-			chThdWait(shelltp);
-		}
-		chThdSleepMilliseconds(1000);
+	
+	while(1) { 
+	  chThdSleepMilliseconds(500);
 	}
+
 }
