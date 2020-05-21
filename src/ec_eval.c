73f67c142ce7b673d7b7735776613634ce580fdf
Joel Svensson <svenssonjoel@yahoo.se>
2020-05-21 19:28:00 +0200

    adding files - work in progress. Planning and experimenting

---
+++ src/ec_eval.c
@@ -0,0 +1,173 @@
+/*
+    Copyright 2020      Joel Svensson	svenssonjoel@yahoo.se
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "symrepr.h"
+#include "heap.h"
+#include "env.h"
+#include "stack.h"
+#include "fundamental.h"
+#include "extensions.h"
+#include "typedefs.h"
+#include "ec_eval.h"
+
+typedef enum {
+  EXP_KIND_ERROR,
+  EXP_SELF_EVALUATING,
+  EXP_VARIABLE,
+  EXP_QUOTED,
+  EXP_DEFINE,
+  EXP_LAMBDA,
+  EXP_PROGN,
+  EXP_IF,
+  EXP_APPLICATION,
+  EXP_LET
+} exp_kind;
+
+typedef enum {
+  CONT_DEFINE,
+} continuation;
+
+
+/* 
+   Machine model:
+   7 x 32 Bit Registers 
+   + Stack
+*/
+typedef struct {
+  uint32_t cont;
+  VALUE env;
+  VALUE unev;
+  VALUE exp;
+  VALUE argl;
+  VALUE val;
+  VALUE fun;
+  
+  stack S;
+} register_machine_t; 
+
+register_machine_t rm_state;
+VALUE ec_eval_global_env;
+
+exp_kind kind_of(VALUE exp) {
+
+  switch (type_of(exp)) {
+  case VAL_TYPE_SYMBOL:
+    if (!is_special(exp))
+      return EXP_VARIABLE;
+    // fall through
+  case PTR_TYPE_BOXED_F:
+  case PTR_TYPE_BOXED_U:
+  case PTR_TYPE_BOXED_I:
+  case VAL_TYPE_I:
+  case VAL_TYPE_U:
+  case VAL_TYPE_CHAR:
+  case PTR_TYPE_ARRAY:
+    return EXP_SELF_EVALUATING;
+  case PTR_TYPE_CONS: {
+    VALUE head = car(rm_state.exp);
+    if (type_of(head) == VAL_TYPE_SYMBOL) {
+      UINT sym_id = dec_sym(head);
+
+      if (sym_id == symrepr_quote())
+	return EXP_QUOTED;
+      if (sym_id == symrepr_define())
+	return EXP_DEFINE;
+      if (sym_id == symrepr_progn())
+	return EXP_PROGN;
+      if (sym_id == symrepr_lambda())
+	return EXP_LAMBDA;
+      if (sym_id == symrepr_if())
+	return EXP_IF;
+      if (sym_id == symrepr_let())
+	return EXP_LET;
+      return EXP_APPLICATION;
+    } // end if symbol
+  } // end case PTR_TYPE_CONS:
+  } 
+  return EXP_KIND_ERROR;
+}
+
+static inline void eval_self_evaluating(bool *eval_continuation) {
+  rm_state.val = rm_state.exp;
+  *eval_continuation = true;
+}
+
+static inline void eval_variable(bool *eval_continuation) {
+  rm_state.val = env_lookup(rm_state.exp, rm_state.env);
+  *eval_continuation = true;
+}
+
+static inline void eval_quoted(bool *eval_continuation) {
+  rm_state.val = car(cdr(rm_state.exp));
+  *eval_continuation = true;
+}
+
+static inline void eval_define(void) {
+  rm_state.unev = car(cdr(rm_state.exp));
+  rm_state.exp  = car(cdr(cdr(rm_state.exp)));
+  push_u32_3(&rm_state.S,
+	     rm_state.unev,
+	     rm_state.env,
+	     rm_state.cont);
+  rm_state.cont = CONT_DEFINE;
+}
+
+static inline void cont_define(bool *eval_continuation) {
+  pop_u32_3(&rm_state.S,
+	    &rm_state.cont,
+	    &rm_state.env,
+	    &rm_state.unev);
+  VALUE new_env = env_set(ec_eval_global_env,
+			  rm_state.unev,
+			  rm_state.val);
+  ec_eval_global_env = new_env; // TODO: GC and error checking.
+  rm_state.val = rm_state.unev;
+  *eval_continuation = true;
+}
+
+void ec_eval(void) {
+
+  bool eval_continuation = false;
+  
+  while (1) {
+
+    if (!eval_continuation) {
+    /* dispatch on exp */
+      switch (kind_of(rm_state.exp)) {
+      case EXP_SELF_EVALUATING:
+	eval_self_evaluating(&eval_continuation);
+	break;
+      case EXP_VARIABLE:
+	eval_variable(&eval_continuation);
+	break;
+      case EXP_QUOTED:
+	eval_quoted(&eval_continuation);
+	break;
+      case EXP_DEFINE:
+	eval_define();
+	break;
+      }
+    } else {
+    /* dispatch on cont */
+      switch (rm_state.cont) {
+      case CONT_DEFINE:
+	cont_define(&eval_continuation);
+	break;
+      }
+    }
+  }
+}
