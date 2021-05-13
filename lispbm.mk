54dd52aed1feff315a468fe9d3a58aeb7621e557
Joel Svensson <svenssonjoel@yahoo.se>
2021-05-13 18:26:40 +0200

    experimentation

--- lispbm.mk
+++ lispbm.mk
@@ -1,9 +1,11 @@
+first_rule: all
 
 include $(LISPBM)/src/lispbm.mk
 
 LISPBM_SRC = $(LISPBM_BASE_SRC)
 LISPBM_H   = $(LISPBM)/include
-LISPBM_FLAGS = 
+LISPBM_FLAGS =
+LISPBM_DEPS  = 
 
 ifeq ($(LISPBM_USE_COMPRESSION), true)
 LISPBM_SRC += $(LISPBM_COMPRESSION_SRC)
@@ -20,6 +22,13 @@ endif
 ifeq ($(LISPBM_USE_PRELUDE), true)
 LISPBM_SRC += $(LISPBM_PRELUDE_SRC)
 LISPBM_FLAGS += -D_PRELUDE
+
+LISPBM_DEPS += $(LISPBM)/src/prelude.xxd
+
+$(LISPBM)/src/prelude.xxd: $(LISPBM)/src/prelude.lisp
+	xxd -i < $(LISPBM)/src/prelude.lisp > $(LISPBM)/src/prelude.xxd 
+
+
 endif
 
 ifeq ($(LISPBM_USE_EXTENSIONS), true)
