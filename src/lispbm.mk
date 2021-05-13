a5c232a41ebe441e82c25edffc56525dc7efc11f
Joel Svensson <svenssonjoel@yahoo.se>
2021-05-13 16:48:38 +0200

    experimenting

--- src/lispbm.mk
+++ src/lispbm.mk
@@ -1,20 +1,20 @@
 
-LISPBM_BASE_SRC := $(LISPBM)/src/env.c \
-                   $(LISPBM)/src/fundamental.c \
-                   $(LISPBM)/src/heap.c \
-                   $(LISPBM)/src/memory.c \
-                   $(LISPBM)/src/print.c \
-                   $(LISPBM)/src/qq_expand.c \
-                   $(LISPBM)/src/stack.c \
-                   $(LISPBM)/src/symrepr.c \
-                   $(LISPBM)/src/tokpar.c
+LISPBM_BASE_SRC = $(LISPBM)/src/env.c \
+                  $(LISPBM)/src/fundamental.c \
+                  $(LISPBM)/src/heap.c \
+                  $(LISPBM)/src/memory.c \
+                  $(LISPBM)/src/print.c \
+                  $(LISPBM)/src/qq_expand.c \
+                  $(LISPBM)/src/stack.c \
+                  $(LISPBM)/src/symrepr.c \
+                  $(LISPBM)/src/tokpar.c
 
-LISPBM_COMPRESSION_SRC := $(LISPBM)/src/compression.c
+LISPBM_COMPRESSION_SRC = $(LISPBM)/src/compression.c
 
-LISPBM_EC_EVAL_SRC := $(LISPBM)/src/ec_eval.c
+LISPBM_EC_EVAL_SRC = $(LISPBM)/src/ec_eval.c
 
-LISPBM_EVAL_CPS_SRC := $(LISPBM)/src/eval_cps.c
+LISPBM_EVAL_CPS_SRC = $(LISPBM)/src/eval_cps.c
 
-LISPBM_PRELUDE_SRC := $(LISPBM)/src/prelude.c 
+LISPBM_PRELUDE_SRC = $(LISPBM)/src/prelude.c 
 
-LISPBM_EXTENSIONS_SRC := $(LISPBM)/src/extensions.c
+LISPBM_EXTENSIONS_SRC = $(LISPBM)/src/extensions.c
