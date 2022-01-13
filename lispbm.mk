1da7b41a302cdee53e39c8fd9cfbbdec4b6fda7e
Joel Svensson <svenssonjoel@yahoo.se>
2022-01-13 10:17:12 +0100

    removing now empty.mk file

--- lispbm.mk
+++ lispbm.mk
@@ -2,7 +2,7 @@ first_rule: all
 
 LISPBM_SRC = $(LISPBM)/src/env.c \
              $(LISPBM)/src/fundamental.c \
-	     $(LISPBM)/src/heap.c \
+             $(LISPBM)/src/heap.c \
              $(LISPBM)/src/lispbm_memory.c \
              $(LISPBM)/src/print.c \
              $(LISPBM)/src/qq_expand.c \
@@ -20,11 +20,11 @@ LISPBM_INC = -I$(LISPBM)/include \
              -I$(LISPBM)/src
 
 LISPBM_FLAGS =
-LISPBM_DEPS  = 
+LISPBM_DEPS  =
 
 LISPBM_FLAGS += -D_PRELUDE
 
 LISPBM_DEPS += $(LISPBM)/src/prelude.xxd
 
 $(LISPBM)/src/prelude.xxd: $(LISPBM)/src/prelude.lisp
-	xxd -i < $(LISPBM)/src/prelude.lisp > $(LISPBM)/src/prelude.xxd 
+        xxd -i < $(LISPBM)/src/prelude.lisp > $(LISPBM)/src/prelude.xxd
