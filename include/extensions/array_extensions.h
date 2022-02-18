2466abc6b88925eb69dc9d85b6833c7c9db1cf0b
Joel Svensson <svenssonjoel@yahoo.se>
2022-02-18 19:10:16 +0100

    work in progress

--- include/extensions/array_extensions.h
+++ include/extensions/array_extensions.h
@@ -18,25 +18,6 @@
 #ifndef ARRAY_EXTENSIONS_H_
 #define ARRAY_EXTENSIONS_H_
 
-
-
-/*
-  void buffer_append_int16(uint8_t* buffer, int16_t number, int32_t *index);
-  void buffer_append_uint16(uint8_t* buffer, uint16_t number, int32_t *index);
-  void buffer_append_int32(uint8_t* buffer, int32_t number, int32_t *index);
-  void buffer_append_uint32(uint8_t* buffer, uint32_t number, int32_t *index);
-  void buffer_append_float16(uint8_t* buffer, float number, float scale, int32_t *index);
-  void buffer_append_float32(uint8_t* buffer, float number, float scale, int32_t *index);
-  void buffer_append_float32_auto(uint8_t* buffer, float number, int32_t *index);
-  int16_t buffer_get_int16(const uint8_t *buffer, int32_t *index);
-  uint16_t buffer_get_uint16(const uint8_t *buffer, int32_t *index);
-  int32_t buffer_get_int32(const uint8_t *buffer, int32_t *index);
-  uint32_t buffer_get_uint32(const uint8_t *buffer, int32_t *index);
-  float buffer_get_float16(const uint8_t *buffer, float scale, int32_t *index);
-  float buffer_get_float32(const uint8_t *buffer, float scale, int32_t *index);
-  float buffer_get_float32_auto(const uint8_t *buffer, int32_t *index);
- */
-
-
+extern bool array_extensions_init(void);
 
 #endif
