0e29e69238f1c2741e0284fdb8bb4f14368a38ad
Joel Svensson <svenssonjoel@yahoo.se>
2022-05-29 20:08:55 +0200

    added SDL example that draws a tree

--- sdlrepl/sdl.lisp
+++ sdlrepl/sdl.lisp
@@ -6,6 +6,24 @@
 (define w 500)
 (define h 500)
 
+(defun degtorad (d)
+  (/ (* d 3.141) 180.0))
+
+(defun rotate (p angle)
+  `(,(- (* (car p) (cos (degtorad angle)))
+        (* (cdr p) (sin (degtorad angle))))
+    .
+    ,(+ (* (car p) (sin (degtorad angle)))
+        (* (cdr p) (cos (degtorad angle))))))
+
+
+(defun trans (p v)
+  `(,(+ (car p) (car v)) . ,(+ (cdr p) (cdr v))))
+
+(defun move (p ang s)
+  (let ((v (rotate `( 0 . ,(- s)) ang)))
+    (trans p v)))
+
 (defun event-loop (w)
   (let ((event (sdl-poll-event)))
     (if (eq event 'sdl-quit-event)
@@ -13,6 +31,20 @@
         (progn  
           (yield 50000)
           (event-loop w)))))
+
+(defun line (rend p1 p2)
+  (sdl-draw-line rend (car p1) (cdr p1) (car p2) (cdr p2)))
+
+
+(defun draw-figure (rend p ang s)
+  (if (<= s 1)
+      ()
+      (let ((p1 (move p ang s)))
+        (progn
+          (line rend p p1)
+          (draw-figure rend p1 (+ ang 2) (- s 1))
+          (draw-figure rend p1 (- ang 27) (/ s 2))
+          (draw-figure rend p1 (+ ang 27) (/ s 2))))))
 
 (defun main ()
   (let ((win (sdl-create-window 500 500))
@@ -22,7 +54,9 @@
       (sdl-renderer-set-color rend 0 0 0)
       (sdl-clear rend)
       (sdl-renderer-set-color rend 255 255 255)
-      (sdl-draw-line rend 0 0 w h)
+
+      (draw-figure rend '(250 . 400) 0 25)
+
       (sdl-present rend)
       'done
       )))
