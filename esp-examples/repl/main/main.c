e65b0b8353d7efe4d35d4561848ffd69321103a8
Joel Svensson <svenssonjoel@yahoo.se>
2022-06-09 14:23:46 +0200

    ESP32c3 repl up and running

--- esp-examples/repl/main/main.c
+++ esp-examples/repl/main/main.c
@@ -156,9 +156,9 @@ void done_callback(eval_context_t *ctx) {
 	int print_ret = lbm_print_value(output, PRINT_SIZE, t);
 
 	if (print_ret >= 0) {
-		uart_printf("<< Context %d finished with value %s >>\r\n# ", cid, output);
+		uart_printf("<< Context %d finished with value %s >>\r\n", cid, output);
 	} else {
-		uart_printf("<< Context %d finished with value %s >>\r\n# ", cid, output);
+		uart_printf("<< Context %d finished with value %s >>\r\n", cid, output);
 	}
 }
 
@@ -202,6 +202,24 @@ lbm_value ext_print(lbm_value *args, lbm_uint argn) {
   return lbm_enc_sym(SYM_TRUE);
 }
 
+static char outbuf[1024];
+
+void print_ctx_info(eval_context_t *ctx, void *arg1, void *arg2) {
+  (void)arg2;
+  lbm_print_value(outbuf, 1024, ctx->r);
+  uart_printf("%s %x %u %u %s\r\n", (char*)arg1, (uint32_t)ctx, ctx->id, ctx->K.sp, outbuf);
+}
+
+void ctx_exists(eval_context_t *ctx, void *arg1, void *arg2) {
+
+  lbm_cid id = *(lbm_cid*)arg1;
+  bool *exists = (bool*)arg2;
+
+  if (ctx->id == id) {
+    *exists = true;
+  }
+}
+
 static char str[1024];
 static char outbuf[1024];
 
@@ -210,7 +228,6 @@ void app_main(void)
 
 	int res = 0;
 	lbm_heap_state_t heap_state;
-
 
 	vTaskDelay(1000);
 	uart_init();
@@ -225,7 +242,7 @@ void app_main(void)
 		uart_printf("LispBM Init failed.\r\n");
 		return;
 	}
-	uart_printf("LispBM Initialized\n");
+	uart_printf("LispBM Initialized\r\n");
 
 	lbm_set_ctx_done_callback(done_callback);
 	lbm_set_timestamp_us_callback(timestamp_callback);
@@ -248,8 +265,7 @@ void app_main(void)
 	);
 
 	if( status == pdPASS ) {
-		uart_printf("Evaluator thread started\n");
-		//vTaskDelete( xHandle );
+		uart_printf("Evaluator thread started\r\n");
 	}
 
 	uart_printf("LispBM Version %d.%d.%d\r\n", LBM_MAJOR_VERSION, LBM_MINOR_VERSION, LBM_PATCH_VERSION);
@@ -275,6 +291,81 @@ void app_main(void)
 			uart_printf("  Free: %u 32Bit words\r\n", lbm_memory_num_free());
 			uart_printf("------------------------------------------------------------\r\n");
 			memset(outbuf,0, 1024);
+		} else if (strncmp(str, ":env", 4) == 0) {
+			lbm_value curr = *lbm_get_env_ptr();
+			uart_printf("Environment:\r\n");
+			while (lbm_type_of(curr) == LBM_TYPE_CONS) {
+				res = lbm_print_value(outbuf,1024, lbm_car(curr));
+				curr = lbm_cdr(curr);
+
+				uart_printf("  %s \r\n", outbuf);
+			}
+		} else if (strncmp(str, ":ctxs", 5) == 0) {
+			lbm_running_iterator(print_ctx_info, "RUNNABLE", NULL);
+			lbm_blocked_iterator(print_ctx_info, "BLOCKED", NULL);
+			lbm_done_iterator   (print_ctx_info, "DONE", NULL);
+		} else if (strncmp(str, ":wait", 5) == 0) {
+			int id = atoi(str + 5);
+			bool exists = false;
+			lbm_done_iterator(ctx_exists, (void*)&id, (void*)&exists);
+			if (exists) {
+				lbm_wait_ctx((lbm_cid)id, WAIT_TIMEOUT);
+			}
+		} else if (strncmp(str, ":pause", 6) == 0) {
+			lbm_pause_eval();
+			while(lbm_get_eval_state() != EVAL_CPS_STATE_PAUSED) {
+				sleep_callback(10);
+			}
+			uart_printf("Evaluator paused\r\nEnter command :continue to unpause or :step to perform single stepping\r\n");
+		} else if (strncmp(str, ":continue", 9) == 0) {
+			lbm_continue_eval();
+		} else if (strncmp(str, ":step", 5) == 0) {
+			lbm_step_eval();
+			while(lbm_get_eval_state() != EVAL_CPS_STATE_PAUSED) {
+				sleep_callback(1);
+			}
+			uart_printf("Evaluator paused\r\nEnter command :continue to unpause or :step to perform single stepping\r\n");
+		} else if (strncmp(str, ":reset", 6) == 0) {
+			lbm_pause_eval();
+			while(lbm_get_eval_state() != EVAL_CPS_STATE_PAUSED) {
+				sleep_callback(1);
+			}
+
+			if (!lbm_init(heap, HEAP_SIZE,
+					gc_stack_storage, GC_STACK_SIZE,
+					memory, LBM_MEMORY_SIZE_8K,
+					bitmap, LBM_MEMORY_BITMAP_SIZE_8K,
+					print_stack_storage, PRINT_STACK_SIZE,
+					extension_storage, EXTENSION_STORAGE_SIZE)) {
+				uart_printf("LispBM Init failed.\r\n");
+				return;
+			}
+			lbm_add_extension("print", ext_print);
+
+		} else if (strncmp(str, ":quit", 5) == 0) {
+
+			break;
+		} else {
+
+			if (strlen(str) == 0) {
+				continue;
+			}
+			/* Get exclusive access to the heap */
+			lbm_pause_eval();
+			while(lbm_get_eval_state() != EVAL_CPS_STATE_PAUSED) {
+				sleep_callback(10);
+			}
+
+			lbm_create_char_stream_from_string(&string_tok_state,
+					&string_tok,
+					str);
+
+			lbm_cid cid = lbm_load_and_eval_expression(&string_tok);
+
+			lbm_continue_eval();
+
+			uart_printf("started ctx: %u\r\n", cid);
+			lbm_wait_ctx((lbm_cid)cid, WAIT_TIMEOUT);
 		}
 	}
 }
