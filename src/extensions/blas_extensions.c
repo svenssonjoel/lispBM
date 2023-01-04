184c756b7fe00d79815d2edbbef9d8e8b31d42b3
Joel Svensson <svenssonjoel@yahoo.se>
2023-01-04 23:02:44 +0100

    added memory leakage tests for string extensions

--- src/extensions/blas_extensions.c
+++ src/extensions/blas_extensions.c
@@ -20,46 +20,75 @@
 #include "lbm_custom_type.h"
 
 static const char *vector_float_desc = "Vector-Float";
+//static const char *matrix_float_desc = "Matrix-Float";
 
 typedef struct {
   unsigned int size;
   float data[1];
 } vector_float_t;
 
-static bool vector_float_destruct(lbm_uint value) {
+static bool common_destructor(lbm_uint value) {
   lbm_free((void*)value);
   return true;
 }
 
-static lbm_value vector_float_allocate(unsigned int size) {
-  vector_float_t *mem = lbm_malloc( 1 * sizeof(unsigned int) +
+static lbm_value vector_float_allocate(lbm_uint size) {
+  vector_float_t *mem = lbm_malloc( 1 * sizeof(lbm_uint) +
                                     size * sizeof(float));
   if (!mem) return ENC_SYM_MERROR;
   mem->size = size;
   lbm_value res;
   lbm_custom_type_create((lbm_uint)mem,
-                         vector_float_destruct,
+                         common_destructor,
                          vector_float_desc,
                          &res);
   return res;
 }
 
+static bool is_vector_float(lbm_value v) {
+  return ((lbm_uint)lbm_get_custom_descriptor(v) == (lbm_uint)vector_float_desc);
+}
+
+/* typedef struct { */
+/*   unsigned int size1; */
+/*   unsigned int size2; */
+/*   float data[1]; */
+/* } matrix_float_t; */
+
+/* static lbm_value matrix_float_allocate(unsigned int size1, unsigned int size2) { */
+/*   matrix_float_t *mem = lbm_malloc(1 * sizeof(lbm_uint) + */
+/*                                    1 * sizeof(lbm_uint) + */
+/*                                    size1 * size2 * sizeof(float)); */
+/*   if (!mem) return ENC_SYM_MERROR; */
+/*   mem->size1 = size1; */
+/*   mem->size2 = size2; */
+/*   lbm_value res; */
+/*   lbm_custom_type_create((lbm_uint)mem, */
+/*                          common_destructor, */
+/*                          matrix_float_desc, */
+/*                          &res); */
+/*   return res; */
+/* } */
+
+/* static bool is_matrix_float(lbm_value m) { */
+/*   return ((lbm_uint)lbm_get_custom_descriptor(m) == (lbm_uint)matrix_float_desc); */
+/* } */
 
 /* **************************************************
- * Extension implementations 
+ * Extension implementations
  */
 
 static lbm_value ext_vector(lbm_value *args, lbm_uint argn) {
 
   LBM_CHECK_NUMBER_ALL();
 
-  if (argn < 1) return ENC_SYM_NIL;
+  if (argn < 1) return ENC_SYM_EERROR;
 
   lbm_value vec = vector_float_allocate(argn);
   if (lbm_is_error(vec)) return vec;
 
   vector_float_t *lvec = (vector_float_t*)lbm_get_custom_value(vec);
-  
+
   for (lbm_uint i = 0; i < argn; i ++) {
     lvec->data[i] = lbm_dec_as_float(args[i]);
   }
@@ -68,9 +97,7 @@ static lbm_value ext_vector(lbm_value *args, lbm_uint argn) {
 
 static lbm_value ext_vector_to_list(lbm_value *args, lbm_uint argn) {
 
-  if (argn != 1  ||
-      ((lbm_uint)lbm_get_custom_descriptor(args[0]) !=
-       (lbm_uint)vector_float_desc)) {
+  if (argn != 1 || !is_vector_float(args[0])) {
     return ENC_SYM_EERROR;
   }
 
@@ -89,7 +116,7 @@ static lbm_value ext_vector_to_list(lbm_value *args, lbm_uint argn) {
       curr = lbm_cdr(curr);
     }
   }
-  
+
   return result;
 }
 
@@ -102,10 +129,8 @@ static lbm_value ext_saxpy(lbm_value *args, lbm_uint argn ) {
   lbm_value x = args[1];
   lbm_value y = args[2];
 
-  if (((lbm_uint)lbm_get_custom_descriptor(x) == (lbm_uint)vector_float_desc) &&
-      ((lbm_uint)lbm_get_custom_descriptor(y) == (lbm_uint)vector_float_desc) &&
-      lbm_is_number(a)) {
-    
+  if (is_vector_float(x) && is_vector_float(y) && lbm_is_number(a)) {
+
     float alpha = lbm_dec_as_float(a);
     vector_float_t *X = (vector_float_t*)lbm_get_custom_value(x);
     vector_float_t *Y = (vector_float_t*)lbm_get_custom_value(y);
@@ -116,7 +141,7 @@ static lbm_value ext_saxpy(lbm_value *args, lbm_uint argn ) {
     if (!lbm_is_symbol_merror(res)) {
 
       vector_float_t *R = (vector_float_t*)lbm_get_custom_value(res);
-      
+
       for (unsigned i = 0; i < res_size; i ++) {
         R->data[i] = alpha * X->data[i] + Y->data[i];
       }
@@ -133,8 +158,7 @@ static lbm_value ext_sdot(lbm_value *args, lbm_uint argn) {
   lbm_value x = args[0];
   lbm_value y = args[1];
 
-  if (((lbm_uint)lbm_get_custom_descriptor(x) == (lbm_uint)vector_float_desc) &&
-      ((lbm_uint)lbm_get_custom_descriptor(y) == (lbm_uint)vector_float_desc)) {
+  if (is_vector_float(x) && is_vector_float(y)) {
 
     vector_float_t *X = (vector_float_t*)lbm_get_custom_value(x);
     vector_float_t *Y = (vector_float_t*)lbm_get_custom_value(y);
@@ -160,6 +184,6 @@ bool lbm_blas_extensions_init(void) {
   res = res && lbm_add_extension("sdot", ext_sdot);
 
   // Matrices
-  
+
   return res;
 }
