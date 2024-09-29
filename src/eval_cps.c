c135760a9e5b87fc60448f0f1d3eccde8e27fe46
Joel Svensson <svenssonjoel@yahoo.se>
2024-09-29 13:42:22 +0200

    added a way to run an arbitrary user callback from the event processing system.

--- src/eval_cps.c
+++ src/eval_cps.c
@@ -254,12 +254,22 @@ static void critical_nonsense(void) {
   return;
 }
 
+static void user_callback_nonsense(void) {
+  return;
+}
+
 static void (*critical_error_callback)(void) = critical_nonsense;
 static void (*usleep_callback)(uint32_t) = usleep_nonsense;
 static uint32_t (*timestamp_us_callback)(void) = timestamp_nonsense;
 static void (*ctx_done_callback)(eval_context_t *) = ctx_done_nonsense;
 static int (*printf_callback)(const char *, ...) = printf_nonsense;
 static bool (*dynamic_load_callback)(const char *, const char **) = dynamic_load_nonsense;
+static void (*user_callback)(void) = user_callback_nonsense;
+
+void lbm_set_user_callback(void (*fptr)(void)) {
+  if (fptr == NULL) user_callback = user_callback_nonsense;
+  else user_callback = fptr;
+}
 
 void lbm_set_critical_error_callback(void (*fptr)(void)) {
   if (fptr == NULL) critical_error_callback = critical_nonsense;
@@ -335,6 +345,10 @@ static bool event_internal(lbm_event_type_t event_type, lbm_uint parameter, lbm_
 
 bool lbm_event_define(lbm_value key, lbm_flat_value_t *fv) {
   return event_internal(LBM_EVENT_DEFINE, key, (lbm_uint)fv->buf, fv->buf_size);
+}
+
+bool lbm_event_run_user_callback(void) {
+  return event_internal(LBM_EVENT_RUN_USER_CALLBACK, 0, 0, 0);
 }
 
 bool lbm_event_unboxed(lbm_value unboxed) {
@@ -5090,6 +5104,9 @@ static void process_events(void) {
       if (lbm_event_handler_pid >= 0) {
         lbm_find_receiver_and_send(lbm_event_handler_pid, event_val);
       }
+      break;
+    case LBM_EVENT_RUN_USER_CALLBACK:
+      user_callback();
       break;
     }
   }
