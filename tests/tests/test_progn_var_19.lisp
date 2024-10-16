888f7057be71afe91a43553ebb06f677020de438
Joel Svensson <svenssonjoel@yahoo.se>
2024-10-16 15:14:26 +0200

    running tests after merging changes to how var and let bindings work

--- tests/tests/test_progn_var_19.lisp
+++ tests/tests/test_progn_var_19.lisp
@@ -1,8 +1,8 @@
 (def lst (list 1 2 3 4))
 
 {
-    (var (a b . rst) lst)
-    (check (= a 1))
-    (check (= b 2))
-    (check (eq rst (list 3 4)))
+(var (a b . rst) lst)
+(check (and (= a 1)
+            (= b 2)
+            (eq rst (list 3 4))))
 }
