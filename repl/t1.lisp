b67ec2e29e76b3a3f17793c62950d03b1ee47243
Joel Svensson <svenssonjoel@yahoo.se>
2025-03-08 16:06:56 +0100

    added t1.lisp images test

---
+++ repl/t1.lisp
@@ -0,0 +1,58 @@
+
+@const-start 
+
+(define f (lambda (x) (print "hello " x)))
+
+(define g (lambda (x) (+ x 1)))
+
+(define l (lambda (n)
+            {
+            (f "apa")
+            (print (g n))
+            (sleep 0.1)
+            (l (+ n 1))
+            }))
+
+(define a1 1)
+(define a2 2u)
+(define a3 3i32)
+(define a4 4u32)
+
+(define ls (list 'a1 'a2 'a3 'a4))
+
+@const-end
+
+(define ls0 (list 1 2 3 ))
+
+@const-start
+
+(define ls1 (cons 100 ls0))
+
+@const-end
+
+(define b1 1)
+(define b2 2u)
+
+(define ls2 (cons 75 ls))
+
+(defun main ()
+  (print "booting image")
+  )
+
+
+(define my-buffer [1 2 3 4 5 6])
+
+
+;; (image-save-env)
+
+;; (define startup '(progn
+;;                    (print (eq (type-of a3) type-i32))
+;;                    (print (eq (type-of a4) type-u32))
+;;                    (print (eq (type-of a1) type-i))
+;;                    (print (eq (type-of a2) type-u))
+;;                    (print ls)))
+
+;; (image-save-startup startup)
+
+(image-save)
+(fwrite-image (fopen "image.lbm" "w"))
