fd0fd6316f3dac095326a5d1a540c3f7079be0f8
Joel Svensson <svenssonjoel@yahoo.se>
2025-03-24 11:16:15 +0100

    adding another arith stress test

---
+++ tests/tests/test_arith_stress_8.lisp
@@ -0,0 +1,16 @@
+
+(defun apply (f args)
+  (eval (cons f args)))
+
+(defun test-it (n c args res acc)
+  (if (= n 0) acc
+  (progn
+    (define acc (and acc (= (apply c args) res)))
+    (test-it (- n 1) c args res acc))))
+
+(defun arith (a b c d e f g h i j)
+  (+ a b c d e f g h i j))
+
+(def res (test-it 100000 arith '(1.0 2.0 3.0 4.0 5.0 6.0 7.0 8.0 9.0 10.0) 55.0 t))
+
+(check res)
