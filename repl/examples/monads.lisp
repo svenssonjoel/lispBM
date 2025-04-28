97b0eb7406abe64d636fb30c0a199f117c4c8b30
Joel Svensson <svenssonjoel@yahoo.se>
2025-04-28 12:10:36 +0200

    adding an example illustrating identity monad and PROGN overlap in functionality

--- repl/examples/monads.lisp
+++ repl/examples/monads.lisp
@@ -49,13 +49,56 @@
            `(>>= ,m ,b (lambda (,a) (do ,m ,xs))))
          ( ((? a) . nil) a)
          ( ((? a) . (? xs))
-           `(progn ,a (do m ,xs)))
+           `(progn ,a (do ,m ,xs)))
          ))
 
-(do listmonad
+(defun test5 ()
+  (do listmonad
     (
      (a <- (list 1 2 3 4))
      (b <- (list 5 6 7 8))
      (mret listmonad (* a b))
      )
-    )
+    ))
+
+;; Generic monad operation
+
+(defun zip-combos (m f ma mb)
+  (do m
+      (
+       (a <- ma)
+       (b <- mb)
+       (mret m (f a b)))))
+
+(defun test6 ()
+  (zip-combos listmonad (lambda (a b) (* a b)) (list 1 2 3 4) (list 5 6 7 8)))
+
+
+;; Identity monad
+
+(define idmonad (make-monad))
+(monad-ret idmonad (lambda (x) x))
+(monad-bind idmonad (lambda (a f) (f a)))
+
+(defun test7 ()
+  (zip-combos idmonad (lambda (a b) (* a b)) 2 10))
+
+;; Identity monad vs progn
+
+(defun test8 ()
+  (do idmonad
+      (
+       (print "hello")
+       (a <- (+ 1 2))
+       (print "the result of (+ 1 2) is " a)
+       )))
+
+(defun test9 () ;; PROGN is { } in LBM
+  {
+  (print "hello")
+  (var a (+ 1 2))
+  (print "the result of (+ 1 2) is " a)
+  }
+  )
+
+
