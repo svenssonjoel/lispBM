6ed18f575f6c2ff11cce42d6d400fdbd94478b24
Joel Svensson <svenssonjoel@yahoo.se>
2025-08-05 21:10:16 +0200

    adding the little linter experiment, better linter to come later

---
+++ linter/test.lisp
@@ -0,0 +1,26 @@
+
+@const-start
+
+@const-end
+
+(if 1 2 3)
+
+(progn (+ 1 2)
+       (+ 3 4))
+
+(define f (lambda (x)
+            (+ x 1))))
+
+
+(define g (lambda (y) (+ y 1000 )))))
+
+apa
+
++
+
+((if £££££))
+
+(423324 ff ff
+
+(dea a++ )
+
