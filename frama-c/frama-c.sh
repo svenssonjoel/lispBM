4289c51d1c54e1831fe5a940add5f6aa8a056a97
Joel Svensson <svenssonjoel@yahoo.se>
2025-09-13 22:00:34 +0200

    tweak frama-c script

--- frama-c/frama-c.sh
+++ frama-c/frama-c.sh
@@ -2,7 +2,7 @@
 
 ## run bear -- make 
 
-frama-c -json-compilation-database compile_commands.json main.c ../src/*.c -save parse.sav
+frama-c -json-compilation-database compile_commands.json main.c ../src/*.c ../platform/linux/src/*.c -save parse.sav
 
 ## frama-c -load parse.sav -eva -eva-precision 3 -save eva.sav
 
