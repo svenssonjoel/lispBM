553256c1a88cc51371305d3330183d7507bc64ea
Joel Svensson <svenssonjoel@yahoo.se>
2025-09-27 09:05:08 +0200

    update platform timestamp for linux

--- platform/linux/src/platform_timestamp.c
+++ platform/linux/src/platform_timestamp.c
@@ -15,8 +15,10 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#define _POSIX_C_SOURCE 200809L // nanosleep?
 #include "platform_timestamp.h"
 #include <sys/time.h>
+#include <time.h>
 #include <unistd.h>
 #include <stdbool.h>
 #include <stdatomic.h>
@@ -28,7 +30,12 @@ void *timestamp_cacher(void *v) {
     struct timeval tv;
     gettimeofday(&tv,NULL);
     atomic_store(&timestamp_cache, (uint32_t)(tv.tv_sec * 1000000 + tv.tv_usec));
-    sleep_callback(100);
+    long us = 100; // sleep 100 us between cache updates.
+    struct timespec s;
+    struct timespec r;
+    s.tv_sec = 0;
+    s.tv_nsec = (long)us * 1000;
+    nanosleep(&s, &r);
   }
 }
 
