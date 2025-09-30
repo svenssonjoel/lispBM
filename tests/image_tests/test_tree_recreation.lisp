387ecdb16f4a6dc29c9d892890cbfedcb9e0b88d
Joel Svensson <svenssonjoel@yahoo.se>
2025-09-30 17:41:09 +0200

    update image tests tree_recreation

--- tests/image_tests/test_tree_recreation.lisp
+++ tests/image_tests/test_tree_recreation.lisp
@@ -5,16 +5,11 @@
 (def t3 (list t2 t2))
 
 (defun main () {
-       ;; t3 recreates as
-       ;; (((1 2 3) 1 2 3) (1 2 3) 1 2 3)
-       ;; which is incorrect.
-       (print t3)
        (if (eq t3 '(((1 2 3) (4 5 6)) ((1 2 3) (4 5 6))))
            (print "SUCCESS")
            (print "FAILURE")
            )
        })
 
-(print t3)
 (image-save)
 (fwrite-image (fopen "image.lbm" "w"))
