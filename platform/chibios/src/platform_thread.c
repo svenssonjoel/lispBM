1f7a52a0ce5ce508ab30d6952cc6b33bdb173e2f
Joel Svensson <svenssonjoel@yahoo.se>
2025-10-22 19:26:31 +0200

    small changes to lbm_midi.c and added platform_thread.c/h for chibios

---
+++ platform/chibios/src/platform_thread.c
@@ -0,0 +1,76 @@
+/*
+    Copyright 2025 Joel Svensson  svenssonjoel@yahoo.se
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "platform_thread.h"
+#include "lbm_memory.h"
+
+bool lbm_thread_create(lbm_thread_t *t,
+                       const char *name,
+                       lbm_thread_func_t func,
+                       void *arg,
+                       lbm_thread_prio_t prio,
+                       uint32_t stack_size) {
+  (void)name;
+
+  platform_thread_t *thread = (platform_thread_t *)t;
+  if (!thread) return false;
+
+  uint32_t wa_size = THD_WORKING_AREA_SIZE(stack_size);
+  void *wa = lbm_malloc(wa_size);
+  if (!wa) {
+    return false;
+  }
+
+  thread->working_area = wa;
+  thread->wa_size = wa_size;
+
+  thread->handle = chThdCreateStatic(wa, wa_size, prio,
+                                     (tfunc_t)func, arg);
+
+  if (!thread->handle) {
+    lbm_free(wa);
+    lbm_free(thread);
+    return false;
+  }
+
+  return true;
+}
+
+void lbm_thread_sleep_us(uint32_t microseconds) {
+  chThdSleepMicroseconds(microseconds);
+}
+
+void lbm_thread_yield(void) {
+  chThdYield();
+}
+
+void lbm_thread_destroy(lbm_thread_t *t) {
+  if (t && *t) {
+    platform_thread_t *thread = *t;
+
+    if (thread->handle) {
+      chThdWait(thread->handle);
+    }
+
+    if (thread->working_area) {
+      lbm_free(thread->working_area);
+    }
+    lbm_free(thread);
+
+    *t = NULL;
+  }
+}
