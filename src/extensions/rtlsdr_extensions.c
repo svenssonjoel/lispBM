8dcb5a7723771e50287ac161294728a862bd7896
Joel Svensson <svenssonjoel@yahoo.se>
2026-01-01 20:10:56 +0100

    refactor to only allow one connected SDR and started thinking about an example radio decoding thread

--- src/extensions/rtlsdr_extensions.c
+++ src/extensions/rtlsdr_extensions.c
@@ -18,18 +18,14 @@
 #include "rtlsdr_extensions.h"
 #include "extensions.h"
 #include "lbm_c_interop.h"
+#include "platform_thread.h"
+#include "platform_timestamp.h"
 
 #include <rtl-sdr.h>
 
-#define RTLSDR_MAX_DEVICES 5
+// supports having a single device open.
 
-typedef struct {
-  rtlsdr_dev_t *dev;
-  uint32_t index;
-  bool     open;
-} rtlsdr_dev_wrapper;
-
-rtlsdr_dev_wrapper devs[RTLSDR_MAX_DEVICES];
+rtlsdr_dev_t *dev = NULL;
 
 static lbm_uint sym_gain_auto;
 static lbm_uint sym_gain_manual;
@@ -40,42 +36,23 @@ lbm_value ext_rtlsdr_open(lbm_value *args, lbm_uint argn) {
   lbm_value r = ENC_SYM_TERROR;
   if (argn == 1 && lbm_is_number(args[0])) {
     uint32_t ix = lbm_dec_as_u32(args[0]);
-    int32_t free_dev = -1;
-    for (int i = 0; i < RTLSDR_MAX_DEVICES; i ++) {
-      if (!devs[i].open) free_dev = i;
-      if (devs[i].open && devs[i].index == ix) { // device enumeration index
-        r = lbm_enc_i(i); // device ID
-        goto rtlsdr_open_done;
-      }
-    }
-    r = ENC_SYM_NIL;
-    if (free_dev >= 0) {
-      devs[free_dev].open = true;
-      devs[free_dev].index = ix;
-      if (rtlsdr_open(&devs[free_dev].dev, ix) == 0) {
-        r = lbm_enc_i(free_dev);
-      }
+    if (rtlsdr_open(&dev, ix) == 0) {
+      r = ENC_SYM_TRUE;
+    } else {
+      r = ENC_SYM_NIL;
     }
   }
- rtlsdr_open_done:
   return r;
 }
 
 lbm_value ext_rtlsdr_close(lbm_value *args, lbm_uint argn) {
+  (void) args;
   lbm_value r = ENC_SYM_TERROR;
-  if (argn == 1 && lbm_is_number(args[0])) {
-    uint32_t dev_id = lbm_dec_as_u32(args[0]);
-    r = ENC_SYM_EERROR;
-    if (dev_id < RTLSDR_MAX_DEVICES) {
-
-      if (devs[dev_id].open) {
-        devs[dev_id].open = false;
-        devs[dev_id].index = 0;
-        // can closing fail? in that case we should have an error field in the
-        // devs array and a way to clean out broken devices.
-        rtlsdr_close(devs[dev_id].dev);
-        r = ENC_SYM_TRUE;
-      }
+  if (argn == 0) {
+    r = ENC_SYM_NIL;
+    if (dev) {
+      rtlsdr_close(dev);
+      r = ENC_SYM_TRUE;
     }
   }
   return r;
@@ -119,12 +96,12 @@ lbm_value ext_rtlsdr_get_device_name(lbm_value *args, lbm_uint argn) {
 
 lbm_value ext_rtlsdr_set_center_freq(lbm_value *args, lbm_uint argn) {
   lbm_value r = ENC_SYM_TERROR;
-  if (argn == 2 && lbm_is_number(args[0]) && lbm_is_number(args[1])) {
-    uint32_t dev_id = lbm_dec_as_u32(args[0]);
-    uint32_t freq = lbm_dec_as_u32(args[1]);
+  if (argn == 1 && lbm_is_number(args[0])) {
+    uint32_t freq = lbm_dec_as_u32(args[0]);
     r = ENC_SYM_EERROR;
-    if (dev_id < RTLSDR_MAX_DEVICES && devs[dev_id].open) {
-      if (rtlsdr_set_center_freq(devs[dev_id].dev, freq) == 0) {
+    if (dev) {
+      r = ENC_SYM_NIL;
+      if (rtlsdr_set_center_freq(dev, freq) == 0) {
         r = ENC_SYM_TRUE;
       }
     }
@@ -133,12 +110,12 @@ lbm_value ext_rtlsdr_set_center_freq(lbm_value *args, lbm_uint argn) {
 }
 
 lbm_value ext_rtlsdr_get_center_freq(lbm_value *args, lbm_uint argn) {
+  (void) args;
   lbm_value r = ENC_SYM_TERROR;
-  if (argn == 1 && lbm_is_number(args[0])) {
-    uint32_t dev_id = lbm_dec_as_u32(args[0]);
+  if (argn == 0) {
     r = ENC_SYM_EERROR;
-    if (dev_id < RTLSDR_MAX_DEVICES && devs[dev_id].open) {
-      uint32_t freq = rtlsdr_get_center_freq(devs[dev_id].dev);
+    if (dev) {
+      uint32_t freq = rtlsdr_get_center_freq(dev);
       r = lbm_enc_u32(freq);
     }
   }
@@ -146,13 +123,14 @@ lbm_value ext_rtlsdr_get_center_freq(lbm_value *args, lbm_uint argn) {
 }
 
 lbm_value ext_rtlsdr_set_sample_rate(lbm_value *args, lbm_uint argn) {
+  (void) args;
   lbm_value r = ENC_SYM_TERROR;
-  if (argn == 2 && lbm_is_number(args[0]) && lbm_is_number(args[1])) {
-    uint32_t dev_id = lbm_dec_as_u32(args[0]);
-    uint32_t rate = lbm_dec_as_u32(args[1]);
+  if (argn == 1 && lbm_is_number(args[0])) {
+    uint32_t rate = lbm_dec_as_u32(args[0]);
     r = ENC_SYM_EERROR;
-    if (dev_id < RTLSDR_MAX_DEVICES && devs[dev_id].open) {
-      if (rtlsdr_set_sample_rate(devs[dev_id].dev, rate) == 0) {
+    if (dev) {
+      r = ENC_SYM_NIL;
+      if (rtlsdr_set_sample_rate(dev, rate) == 0) {
         r = ENC_SYM_TRUE;
       }
     }
@@ -161,12 +139,12 @@ lbm_value ext_rtlsdr_set_sample_rate(lbm_value *args, lbm_uint argn) {
 }
 
 lbm_value ext_rtlsdr_get_sample_rate(lbm_value *args, lbm_uint argn) {
+  (void) args;
   lbm_value r = ENC_SYM_TERROR;
-  if (argn == 1 && lbm_is_number(args[0])) {
-    uint32_t dev_id = lbm_dec_as_u32(args[0]);
+  if (argn == 0) {
     r = ENC_SYM_EERROR;
-    if (dev_id < RTLSDR_MAX_DEVICES && devs[dev_id].open) {
-      uint32_t rate = rtlsdr_get_sample_rate(devs[dev_id].dev);
+    if (dev) {
+      uint32_t rate = rtlsdr_get_sample_rate(dev);
       r = lbm_enc_u32(rate);
     }
   }
@@ -175,18 +153,17 @@ lbm_value ext_rtlsdr_get_sample_rate(lbm_value *args, lbm_uint argn) {
 
 lbm_value ext_rtlsdr_set_tuner_gain_mode(lbm_value *args, lbm_uint argn) {
   lbm_value r = ENC_SYM_TERROR;
-  if (argn == 2 && lbm_is_number(args[0]) && lbm_is_symbol(args[1])) {
-    uint32_t dev_id = lbm_dec_as_u32(args[0]);
-    int32_t mode = lbm_dec_sym(args[1]);
+  if (argn == 1 && lbm_is_symbol(args[0])) {
+    int32_t mode = lbm_dec_sym(args[0]);
     r = ENC_SYM_EERROR;
-    if (dev_id < RTLSDR_MAX_DEVICES && devs[dev_id].open) {
+    if (dev) {
       r = ENC_SYM_NIL;
       if (mode == sym_gain_manual) {
-        if (rtlsdr_set_tuner_gain_mode(devs[dev_id].dev, 1) == 0) {
+        if (rtlsdr_set_tuner_gain_mode(dev, 1) == 0) {
           r = ENC_SYM_TRUE;
         }
       } else if (mode == sym_gain_auto) {
-        if (rtlsdr_set_tuner_gain_mode(devs[dev_id].dev, 0) == 0) {
+        if (rtlsdr_set_tuner_gain_mode(dev, 0) == 0) {
           r = ENC_SYM_TRUE;
         }
       }
@@ -197,12 +174,12 @@ lbm_value ext_rtlsdr_set_tuner_gain_mode(lbm_value *args, lbm_uint argn) {
 
 lbm_value ext_rtlsdr_set_tuner_gain(lbm_value *args, lbm_uint argn) {
   lbm_value r = ENC_SYM_TERROR;
-  if (argn == 2 && lbm_is_number(args[0]) && lbm_is_number(args[1])) {
-    uint32_t dev_id = lbm_dec_as_u32(args[0]);
-    int32_t gain = lbm_dec_as_i32(args[1]);
+  if (argn == 1 && lbm_is_number(args[0])) {
+    int32_t gain = lbm_dec_as_i32(args[0]);
     r = ENC_SYM_EERROR;
-    if (dev_id < RTLSDR_MAX_DEVICES && devs[dev_id].open) {
-      if (rtlsdr_set_tuner_gain(devs[dev_id].dev, gain) == 0) {
+    if (dev) {
+      r = ENC_SYM_NIL;
+      if (rtlsdr_set_tuner_gain(dev, gain) == 0) {
         r = ENC_SYM_TRUE;
       }
     }
@@ -211,12 +188,12 @@ lbm_value ext_rtlsdr_set_tuner_gain(lbm_value *args, lbm_uint argn) {
 }
 
 lbm_value ext_rtlsdr_get_tuner_gain(lbm_value *args, lbm_uint argn) {
+  (void) args;
   lbm_value r = ENC_SYM_TERROR;
-  if (argn == 1 && lbm_is_number(args[0])) {
-    uint32_t dev_id = lbm_dec_as_u32(args[0]);
+  if (argn == 0) {
     r = ENC_SYM_EERROR;
-    if (dev_id < RTLSDR_MAX_DEVICES && devs[dev_id].open) {
-      int32_t gain = rtlsdr_get_tuner_gain(devs[dev_id].dev);
+    if (dev) {
+      int32_t gain = rtlsdr_get_tuner_gain(dev);
       r = lbm_enc_i32(gain);
     }
   }
@@ -225,18 +202,17 @@ lbm_value ext_rtlsdr_get_tuner_gain(lbm_value *args, lbm_uint argn) {
 
 lbm_value ext_rtlsdr_set_agc_mode(lbm_value *args, lbm_uint argn) {
   lbm_value r = ENC_SYM_TERROR;
-  if (argn == 2 && lbm_is_number(args[0]) && lbm_is_symbol(args[1])) {
-    uint32_t dev_id = lbm_dec_as_u32(args[0]);
-    int32_t mode = lbm_dec_sym(args[1]);
+  if (argn == 1 && lbm_is_symbol(args[0])) {
+    int32_t mode = lbm_dec_sym(args[0]);
     r = ENC_SYM_EERROR;
-    if (dev_id < RTLSDR_MAX_DEVICES && devs[dev_id].open) {
+    if (dev) {
       r = ENC_SYM_NIL;
       if (mode == sym_agc_on) {
-        if (rtlsdr_set_agc_mode(devs[dev_id].dev, 1) == 0) {
+        if (rtlsdr_set_agc_mode(dev, 1) == 0) {
           r = ENC_SYM_TRUE;
         }
       } else if (mode == sym_agc_off) {
-        if (rtlsdr_set_agc_mode(devs[dev_id].dev, 0) == 0) {
+        if (rtlsdr_set_agc_mode(dev, 0) == 0) {
           r = ENC_SYM_TRUE;
         }
       }
@@ -247,6 +223,7 @@ lbm_value ext_rtlsdr_set_agc_mode(lbm_value *args, lbm_uint argn) {
 
 // Towards FM demodulation
 
+static lbm_thread_t radio_thread;
 
 // audio_out is the same size as the i_data/q_data input arrays.
 static void fm_discriminator(float *i_data,
@@ -263,13 +240,54 @@ static void fm_discriminator(float *i_data,
 }
 
 
+static volatile bool radio_thread_running = false;
+
+static void radio_thd(void *arg) {
+  (void) arg;
+
+  while (radio_thread_running) {
+
+    lbm_thread_sleep_us(100);
+  }
+
+}
+
+lbm_value ext_rtlsdr_start_radio_thd(lbm_value *args, lbm_uint argn) {
+  (void) args;
+  lbm_value r = ENC_SYM_TERROR;
+  if (argn == 0) {
+    r = ENC_SYM_TRUE;
+    if (!radio_thread_running) {
+      radio_thread_running = true;
+      if (!lbm_thread_create(&radio_thread, "radio_thd", radio_thd,
+                             NULL, LBM_THREAD_PRIO_NORMAL, 8192)) {
+        r = ENC_SYM_NIL;
+      }
+    }
+  }
+  return r;
+}
+
+lbm_value ext_rtlsdr_stop_radio_thd(lbm_value *args, lbm_uint argn) {
+  (void) args;
+  lbm_value r = ENC_SYM_TERROR;
+  if (argn == 0) {
+    r = ENC_SYM_NIL;
+    if (radio_thread_running) {
+      radio_thread_running = false;
+      lbm_thread_destroy(&radio_thread);
+      r = ENC_SYM_TRUE;
+    }
+  }
+  return r;
+}
+
+
 void lbm_rtlsdr_extensions_init(void) {
 
-  for (int i = 0; i < RTLSDR_MAX_DEVICES; i ++) {
-    devs[i].dev = NULL;
-    devs[i].index = 0;
-    devs[i].open = false;
-  }
+  dev = NULL;
+
+  radio_thread_running = false;
 
   lbm_add_symbol_const("gain-mode-auto", &sym_gain_auto);
   lbm_add_symbol_const("gain-mode-manual", &sym_gain_manual);
@@ -288,4 +306,7 @@ void lbm_rtlsdr_extensions_init(void) {
   lbm_add_extension("rtlsdr-set-tuner-gain", ext_rtlsdr_set_tuner_gain);
   lbm_add_extension("rtlsdr-get-tuner-gain", ext_rtlsdr_get_tuner_gain);
   lbm_add_extension("rtlsdr-set-agc-mode", ext_rtlsdr_set_agc_mode);
+
+  lbm_add_extension("rtlsdr-start-radio-thread", ext_rtlsdr_start_radio_thd);
+  lbm_add_extension("rtlsdr-stop-radio-thread", ext_rtlsdr_stop_radio_thd);
 }
