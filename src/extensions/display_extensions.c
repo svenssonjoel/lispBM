4e5a1272574c7aebaf9163d5f1e02e6243a90e01
DovPearX <103691298+DovPearX@users.noreply.github.com>
2026-07-26 18:34:27 +0200

    Update fonts

--- src/extensions/display_extensions.c
+++ src/extensions/display_extensions.c
@@ -1109,7 +1109,6 @@ static bool font_data_is_valid(const lbm_array_header_t *font) {
 // lisp args:
 //   img x y fg bg font str  [attrs] ['up|'down]
 //   img x y fg bg font-id str [attrs] ['up|'down]  uses a compiled-in font
-//   img x y fg bg str       [attrs] ['up|'down]   uses the configured default font
 //   img x y '(c0..c3) font str [attrs] ['up|'down]  4-color form for 2bpp fonts
 // attrs: '(magnify N) '(scale N) '(spacing N) '(align 'left|'center|'right) '(rotate deg)
 // orient: 0=normal 1=up/90CCW 2=180 3=down/90CW
@@ -1137,7 +1136,7 @@ static lbm_value ext_text(lbm_value *args, lbm_uint argn) {
     break;
   }
 
-  if (core_argn < 6 || core_argn > 7) return ENC_SYM_TERROR;
+  if (core_argn != 6 && core_argn != 7) return ENC_SYM_TERROR;
 
   if (rot_deg != 0) {
     orient = (((rot_deg % 360) + 360) % 360) / 90;
@@ -1147,7 +1146,7 @@ static lbm_value ext_text(lbm_value *args, lbm_uint argn) {
   int y = lbm_dec_as_i32(args[2]);
 
   int32_t colors[4] = {-1, -1, -1, -1};
-  const uint8_t *font_data = tinygfx_get_builtin_font(LBM_DISPLAY_DEFAULT_FONT);
+  const uint8_t *font_data = NULL;
   char *txt = NULL;
 
   if (lbm_is_cons(args[3])) {
@@ -1190,7 +1189,7 @@ static lbm_value ext_text(lbm_value *args, lbm_uint argn) {
       }
       txt = lbm_dec_str(args[6]);
     } else {
-      txt = lbm_dec_str(args[5]);
+      return ENC_SYM_TERROR;
     }
   } else {
     return ENC_SYM_TERROR;
